7352a82622f01b574a261547ea3fd06c2e6c9fce
Michał Dolaś <me@shockah.pl>
2023-11-22 21:38:08 +0100

    CodexHelper: New mod

---
+++ CodexHelper/ModEntry.cs
@@ -0,0 +1,23 @@
+﻿using CobaltCoreModding.Definitions;
+using CobaltCoreModding.Definitions.ModContactPoints;
+using CobaltCoreModding.Definitions.ModManifests;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Shockah.CodexHelper;
+
+public sealed class ModEntry : IModManifest
+{
+	public string Name { get; init; } = typeof(ModEntry).FullName!;
+	public IEnumerable<DependencyEntry> Dependencies => Array.Empty<DependencyEntry>();
+
+	public DirectoryInfo? GameRootFolder { get; set; }
+	public DirectoryInfo? ModRootFolder { get; set; }
+	public ILogger? Logger { get; set; }
+
+	public void BootMod(IModLoaderContact contact)
+	{
+	}
+}
