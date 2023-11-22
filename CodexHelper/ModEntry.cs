df5c7865119a09bdf44319d34ec2d0f786e40d67
Michał Dolaś <me@shockah.pl>
2023-11-22 23:08:53 +0100

    CodexHelper: Actual implementation

--- CodexHelper/ModEntry.cs
+++ CodexHelper/ModEntry.cs
@@ -1,7 +1,10 @@
 ﻿using CobaltCoreModding.Definitions;
 using CobaltCoreModding.Definitions.ModContactPoints;
 using CobaltCoreModding.Definitions.ModManifests;
+using daisyowl.text;
+using HarmonyLib;
 using Microsoft.Extensions.Logging;
+using Shockah.Shared;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,7 +20,54 @@ public sealed class ModEntry : IModManifest
 	public DirectoryInfo? ModRootFolder { get; set; }
 	public ILogger? Logger { get; set; }
 
+	private static bool IsRenderingCardRewardMenu = false;
+
 	public void BootMod(IModLoaderContact contact)
 	{
+		Harmony harmony = new(Name);
+		harmony.TryPatch(
+			logger: Logger!,
+			original: () => AccessTools.DeclaredMethod(typeof(CardReward), nameof(CardReward.Render)),
+			prefix: new HarmonyMethod(GetType(), nameof(CardReward_Render_Prefix)),
+			finalizer: new HarmonyMethod(GetType(), nameof(CardReward_Render_Finalizer))
+		);
+		harmony.TryPatch(
+			logger: Logger!,
+			original: () => AccessTools.DeclaredMethod(typeof(Card), nameof(Card.Render)),
+			postfix: new HarmonyMethod(GetType(), nameof(Card_Render_Postfix))
+		);
+		harmony.TryPatch(
+			logger: Logger!,
+			original: () => AccessTools.DeclaredMethod(typeof(ArtifactReward), nameof(ArtifactReward.Render)),
+			postfix: new HarmonyMethod(GetType(), nameof(ArtifactReward_Render_Postfix))
+		);
+	}
+
+	private static void CardReward_Render_Prefix()
+		=> IsRenderingCardRewardMenu = true;
+
+	private static void CardReward_Render_Finalizer()
+		=> IsRenderingCardRewardMenu = false;
+
+	private static void Card_Render_Postfix(Card __instance, G g)
+	{
+		if (!IsRenderingCardRewardMenu)
+			return;
+		if (g.state.storyVars.cardsOwned.Contains(__instance.Key()))
+			return;
+
+		var rect = __instance.GetScreenRect();
+		Draw.Text(Loc.T("codex.title", "CODEX"), __instance.targetPos.x + rect.w / 2 - 2, __instance.targetPos.y - 8, DB.pinch, Colors.textMain, align: TAlign.Center, extraScale: 0.75);
+	}
+
+	private static void ArtifactReward_Render_Postfix(ArtifactReward __instance, G g)
+	{
+		foreach (var artifact in __instance.artifacts)
+		{
+			if (g.state.storyVars.artifactsOwned.Contains(artifact.Key()))
+				continue;
+
+			Draw.Text(Loc.T("codex.title", "CODEX"), artifact.lastScreenPos.x + 170, artifact.lastScreenPos.y + 12, DB.pinch, Colors.textMain, align: TAlign.Right, extraScale: 0.75);
+		}
 	}
 }
---
+++ _Shared/HarmonyExt.cs
@@ -0,0 +1,232 @@
+using HarmonyLib;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Shockah.Shared;
+
+public static class HarmonyExt
+{
+	private static void WarnOnDebugAssembly(ILogger logger, Assembly? assembly)
+	{
+		if (assembly?.IsBuiltInDebugConfiguration() == true)
+			logger.LogWarning("{Assembly} was built in debug configuration - patching may fail. If it does fail, please ask that mod's developer to build it in release configuration.", assembly.GetName().Name);
+	}
+
+	public static void Patch(
+		this Harmony self,
+		MethodBase? original,
+		ILogger logger,
+		LogLevel problemLogLevel = LogLevel.Error,
+		LogLevel successLogLevel = LogLevel.Trace,
+		HarmonyMethod? prefix = null,
+		HarmonyMethod? postfix = null,
+		HarmonyMethod? transpiler = null,
+		HarmonyMethod? finalizer = null
+	)
+	{
+		if (original is null)
+		{
+			logger.Log(problemLogLevel, "Could not patch method - the mod may not work correctly.\nReason: Unknown method to patch.");
+			return;
+		}
+		if (transpiler is not null)
+			WarnOnDebugAssembly(logger, original.DeclaringType?.Assembly);
+		self.Patch(original, prefix, postfix, transpiler, finalizer);
+		logger.Log(successLogLevel, "Patched method {Method}.", original.FullDescription());
+	}
+
+	public static bool TryPatch(
+		this Harmony self,
+		Func<MethodBase?> original,
+		ILogger logger,
+		LogLevel problemLogLevel = LogLevel.Error,
+		LogLevel successLogLevel = LogLevel.Trace,
+		HarmonyMethod? prefix = null,
+		HarmonyMethod? postfix = null,
+		HarmonyMethod? transpiler = null,
+		HarmonyMethod? finalizer = null
+	)
+	{
+		var originalMethod = original();
+		if (originalMethod is null)
+		{
+			logger.Log(problemLogLevel, "Could not patch method - the mod may not work correctly.\nReason: Unknown method to patch.");
+			return false;
+		}
+
+		try
+		{
+
+			if (transpiler is not null)
+				WarnOnDebugAssembly(logger, originalMethod.DeclaringType?.Assembly);
+			self.Patch(originalMethod, prefix, postfix, transpiler, finalizer);
+			logger.Log(successLogLevel, "Patched method {Method}.", originalMethod.FullDescription());
+			return true;
+		}
+		catch (Exception ex)
+		{
+			logger.Log(problemLogLevel, "Could not patch method {Method} - the mod may not work correctly.\nReason: {Exception}", originalMethod, ex);
+			return false;
+		}
+	}
+
+	public static void PatchVirtual(
+		this Harmony self,
+		MethodBase? original,
+		ILogger logger,
+		LogLevel problemLogLevel = LogLevel.Error,
+		LogLevel successLogLevel = LogLevel.Trace,
+		HarmonyMethod? prefix = null,
+		HarmonyMethod? postfix = null,
+		HarmonyMethod? transpiler = null,
+		HarmonyMethod? finalizer = null
+	)
+	{
+		if (original is null)
+		{
+			logger.Log(problemLogLevel, "Could not patch method - the mod may not work correctly.\nReason: Unknown method to patch.");
+			return;
+		}
+
+		Type? declaringType = original.DeclaringType;
+		if (declaringType == null)
+			throw new ArgumentException($"{nameof(original)}.{nameof(original.DeclaringType)} is null.");
+		foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+		{
+			IEnumerable<Type> subtypes = Enumerable.Empty<Type>();
+			try
+			{
+				subtypes = assembly.GetTypes().Where(t => t.IsAssignableTo(declaringType));
+			}
+			catch (Exception ex)
+			{
+				logger.Log(LogLevel.Trace, "There was a problem while getting types defined in assembly {Assembly}, ignoring it. Reason:\n{Exception}", assembly.GetName().Name, ex);
+			}
+
+			foreach (Type subtype in subtypes)
+			{
+				var originalParameters = original.GetParameters();
+				var subtypeOriginal = AccessTools.Method(
+					subtype,
+					original.Name,
+					originalParameters.Select(p => p.ParameterType).ToArray()
+				);
+				if (subtypeOriginal is null)
+					continue;
+				if (!subtypeOriginal.IsDeclaredMember())
+					continue;
+				if (!subtypeOriginal.HasMethodBody())
+					continue;
+
+				static bool ContainsNonSpecialArguments(HarmonyMethod patch)
+					=> patch.method.GetParameters().Any(p => !(p.Name ?? "").StartsWith("__"));
+
+				if (
+					(prefix is not null && ContainsNonSpecialArguments(prefix)) ||
+					(postfix is not null && ContainsNonSpecialArguments(postfix)) ||
+					(finalizer is not null && ContainsNonSpecialArguments(finalizer))
+				)
+				{
+					var subtypeOriginalParameters = subtypeOriginal.GetParameters();
+					for (int i = 0; i < original.GetParameters().Length; i++)
+						if (originalParameters[i].Name != subtypeOriginalParameters[i].Name)
+							throw new InvalidOperationException($"Method {declaringType.Name}.{original.Name} cannot be automatically patched for subtype {subtype.Name}, because argument #{i} has a mismatched name: `{originalParameters[i].Name}` vs `{subtypeOriginalParameters[i].Name}`.");
+				}
+
+				self.Patch(subtypeOriginal, prefix, subtypeOriginal.HasMethodBody() ? postfix : null, transpiler, finalizer);
+				logger.Log(successLogLevel, "Patched method {Method}.", subtypeOriginal.FullDescription());
+			}
+		}
+	}
+
+	public static (int patched, int total) TryPatchVirtual(
+		this Harmony self,
+		Func<MethodBase?> original,
+		ILogger logger,
+		LogLevel problemLogLevel = LogLevel.Error,
+		LogLevel successLogLevel = LogLevel.Trace,
+		HarmonyMethod? prefix = null,
+		HarmonyMethod? postfix = null,
+		HarmonyMethod? transpiler = null,
+		HarmonyMethod? finalizer = null
+	)
+	{
+		var originalMethod = original();
+		if (originalMethod is null)
+		{
+			logger.Log(problemLogLevel, "Could not patch method - the mod may not work correctly.\nReason: Unknown method to patch.");
+			return (0, 1);
+		}
+
+		try
+		{
+			int patched = 0;
+			int total = 0;
+			Type declaringType = originalMethod.DeclaringType ?? throw new ArgumentException($"{nameof(original)}.{nameof(originalMethod.DeclaringType)} is null.");
+			foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				total++;
+				IEnumerable<Type> subtypes = Enumerable.Empty<Type>();
+				try
+				{
+					subtypes = assembly.GetTypes().Where(t => t.IsAssignableTo(declaringType));
+				}
+				catch (Exception ex)
+				{
+					logger.Log(LogLevel.Trace, "There was a problem while getting types defined in assembly {Assembly}, ignoring it. Reason:\n{Exception}", assembly.GetName().Name, ex);
+				}
+
+				foreach (Type subtype in subtypes)
+				{
+					try
+					{
+						var originalParameters = originalMethod.GetParameters();
+						var subtypeOriginal = AccessTools.Method(
+							subtype,
+							originalMethod.Name,
+							originalParameters.Select(p => p.ParameterType).ToArray()
+						);
+						if (subtypeOriginal is null)
+							continue;
+						if (!subtypeOriginal.IsDeclaredMember())
+							continue;
+						if (!subtypeOriginal.HasMethodBody())
+							continue;
+
+						static bool ContainsNonSpecialArguments(HarmonyMethod patch)
+							=> patch.method.GetParameters().Any(p => !(p.Name ?? "").StartsWith("__"));
+
+						if (
+							(prefix is not null && ContainsNonSpecialArguments(prefix)) ||
+							(postfix is not null && ContainsNonSpecialArguments(postfix)) ||
+							(finalizer is not null && ContainsNonSpecialArguments(finalizer))
+						)
+						{
+							var subtypeOriginalParameters = subtypeOriginal.GetParameters();
+							for (int i = 0; i < originalMethod.GetParameters().Length; i++)
+								if (originalParameters[i].Name != subtypeOriginalParameters[i].Name)
+									throw new InvalidOperationException($"Method {declaringType.Name}.{originalMethod.Name} cannot be automatically patched for subtype {subtype.Name}, because argument #{i} has a mismatched name: `{originalParameters[i].Name}` vs `{subtypeOriginalParameters[i].Name}`.");
+						}
+
+						self.Patch(subtypeOriginal, prefix, postfix, transpiler, finalizer);
+						logger.Log(successLogLevel, "Patched method {Method}.", subtypeOriginal.FullDescription());
+						patched++;
+					}
+					catch (Exception ex)
+					{
+						logger.Log(problemLogLevel, "Could not patch method - the mod may not work correctly.\nReason: {Exception}", ex);
+					}
+				}
+			}
+			return (patched, total);
+		}
+		catch (Exception ex)
+		{
+			logger.Log(problemLogLevel, "Could not patch method {Method} - the mod may not work correctly.\nReason: {Exception}", originalMethod, ex);
+			return (0, 1);
+		}
+	}
+}
