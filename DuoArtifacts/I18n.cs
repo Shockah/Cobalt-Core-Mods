4fa87a951cfcb64ce95680de68fa9c604a724bc2
Michał Dolaś <me@shockah.pl>
2023-11-27 00:54:44 +0100

    DuoArtifacts: Improved tooltips

--- DuoArtifacts/I18n.cs
+++ DuoArtifacts/I18n.cs
@@ -5,17 +5,17 @@ internal static class I18n
 	public static string DuoArtifactDeckName => "Duo";
 
 	public static string BooksDrakeArtifactName => "Books-Drake Duo Artifact";
-	public static string BooksDrakeArtifactTooltip => "Whenever you play an attack, lose 2 <c=status>SHARD</c>: the attack becomes piercing. If it was already piercing, it stuns. If it was already stunning, <c=status>TOTAL STUN</c> the opponent.";
+	public static string BooksDrakeArtifactTooltip => "Whenever you play an attack, lose 2 <c=shard>SHARD</c>: the attack becomes piercing. If it was already piercing, it stuns. If it was already stunning, <c=eunice>TOTAL STUN</c> the opponent.";
 
 	public static string DizzyDrakeArtifactName => "Dizzy-Drake Duo Artifact";
-	public static string DizzyDrakeArtifactTooltip => "<c=status>OVERHEAT</c> can now reduce <c=status>(TEMP) SHIELD</c> instead of hull.";
+	public static string DizzyDrakeArtifactTooltip => "<c=eunice>OVERHEAT</c> can now reduce <c=dizzy>(TEMP) SHIELD</c> instead of hull.";
 
 	public static string DrakePeriArtifactName => "Drake-Peri Duo Artifact";
-	public static string DrakePeriArtifactTooltip => "Whenever you <c=status>OVERHEAT</c>, convert your <c=status>OVERDRIVE</c> into <c=status>POWERDRIVE</c>.";
+	public static string DrakePeriArtifactTooltip => "Whenever you <c=eunice>OVERHEAT</c>, convert your <c=peri>OVERDRIVE</c> into <c=peri>POWERDRIVE</c>.";
 
 	public static string IsaacPeriArtifactName => "Isaac-Peri Duo Artifact";
-	public static string IsaacPeriArtifactTooltip => "Your drones benefit from <c=status>POWERDRIVE</c>, <c=status>OVERDRIVE</c> and <c=status>FLUX</c>.";
+	public static string IsaacPeriArtifactTooltip => "Your <c=goat>Attack Drones</c> benefit from <c=peri>POWERDRIVE</c>, <c=peri>OVERDRIVE</c> and <c=peri>FLUX</c>.";
 
 	public static string IsaacRiggsArtifactName => "Isaac-Riggs Duo Artifact";
-	public static string IsaacRiggsArtifactTooltip => "<c=status>EVADE</c> and <c=status>DRONESHIFT</c> can be used interchangeably.\nGain 1 <c=status>EVADE</c> on the first turn.";
+	public static string IsaacRiggsArtifactTooltip => "<c=riggs>EVADE</c> and <c=goat>DRONESHIFT</c> can be used interchangeably.\nGain 1 <c=riggs>EVADE</c> on the first turn.";
 }
