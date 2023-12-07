bbc347a7bfc71c89da41706dee458be35596b956
Michał Dolaś <me@shockah.pl>
2023-12-07 01:36:37 +0100

    DuoArtifacts: Drake-Riggs inactive sprite

--- DuoArtifacts/Artifacts/DrakeRiggsArtifact.cs
+++ DuoArtifacts/Artifacts/DrakeRiggsArtifact.cs
@@ -1,10 +1,16 @@
-﻿using HarmonyLib;
+﻿using CobaltCoreModding.Definitions.ExternalItems;
+using CobaltCoreModding.Definitions.ModContactPoints;
+using HarmonyLib;
+using Shockah.Shared;
+using System.IO;
 using System.Linq;
 
 namespace Shockah.DuoArtifacts;
 
 internal sealed class DrakeRiggsArtifact : DuoArtifact, IEvadeHook
 {
+	internal static ExternalSprite InactiveSprite { get; private set; } = null!;
+
 	public bool UsedThisTurn = false;
 
 	protected internal override void ApplyPatches(Harmony harmony)
@@ -12,6 +18,18 @@ internal sealed class DrakeRiggsArtifact : DuoArtifact, IEvadeHook
 		base.ApplyPatches(harmony);
 		Instance.KokoroApi.RegisterEvadeHook(this, -10);
 	}
+
+	protected internal override void RegisterArt(ISpriteRegistry registry, string namePrefix, DuoArtifactDefinition definition)
+	{
+		base.RegisterArt(registry, namePrefix, definition);
+		InactiveSprite = registry.RegisterArtOrThrow(
+			id: $"{typeof(ModEntry).Namespace}.Artifact.{string.Join("_", definition.CharacterKeys.Value.OrderBy(key => key))}.Inactive",
+			file: new FileInfo(Path.Combine(Instance.ModRootFolder!.FullName, "assets", "Artifacts", "DrakeRiggsInactive.png"))
+		);
+	}
+
+	public override Spr GetSprite()
+		=> UsedThisTurn ? (Spr)InactiveSprite.Id!.Value : base.GetSprite();
 
 	public override void OnTurnStart(State state, Combat combat)
 	{
