39d59ae3b02770c1c459b792d6f5953b1df5d3db
Michał Dolaś <me@shockah.pl>
2023-12-14 10:28:57 +0100

    Soggins: Botches are no longer affected by Boost

--- Soggins/StatusLogicManager.cs
+++ Soggins/StatusLogicManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Shockah.Soggins;
+﻿namespace Shockah.Soggins;
 
 internal sealed class StatusLogicManager : IStatusLogicHook
 {
@@ -11,12 +9,6 @@ internal sealed class StatusLogicManager : IStatusLogicHook
 		Instance.KokoroApi.RegisterStatusLogicHook(this, 0);
 	}
 
-	public IEnumerable<(Status Status, double Priority)> GetExtraStatusesToShow(State state, Combat combat, Ship ship)
-	{
-		if (Instance.Api.GetSmug(ship) is not null)
-			yield return (Status: (Status)Instance.SmugStatus.Id!.Value, Priority: 10);
-	}
-
 	public bool? IsAffectedByBoost(State state, Combat combat, Ship ship, Status status)
-		=> status == (Status)Instance.DoubleTimeStatus.Id!.Value ? false : null;
+		=> status == (Status)Instance.DoubleTimeStatus.Id!.Value || status == (Status)Instance.BotchesStatus.Id!.Value ? false : null;
 }
