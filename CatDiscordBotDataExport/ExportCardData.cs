81cb783b2d2e5c624a95719b02480b4aab720986
Michał Dolaś <me@shockah.pl>
2023-12-17 18:17:28 +0100

    CatDataExport: WIP tool mod

---
+++ CatDiscordBotDataExport/ExportCardData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Shockah.CatDiscordBotDataExport;
+
+internal sealed record class ExportCardData(
+	string Key,
+	string Name,
+	bool Unreleased,
+	Rarity Rarity,
+	IReadOnlySet<Upgrade> Upgrades,
+	string? Description
+);
