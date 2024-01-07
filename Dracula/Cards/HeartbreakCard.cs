93ff54f88ae6fa8e1fee7ff50edde5fbf0dfd5ba
Michał Dolaś <me@shockah.pl>
2024-01-08 00:34:40 +0100

    Dracula: More cards

---
+++ Dracula/Cards/HeartbreakCard.cs
@@ -0,0 +1,77 @@
+﻿using Nickel;
+using System.Collections.Generic;
+
+namespace Shockah.Dracula;
+
+internal sealed class HeartbreakCard : Card, IRegisterableCard
+{
+	public void Register(IModHelper helper)
+	{
+		helper.Content.Cards.RegisterCard("Heartbreak", new()
+		{
+			CardType = GetType(),
+			Meta = new()
+			{
+				deck = ModEntry.Instance.DraculaDeck.Deck,
+				rarity = Rarity.uncommon,
+				upgradesTo = [Upgrade.A, Upgrade.B]
+			},
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Heartbreak", "name"]).Localize
+		});
+	}
+
+	public override CardData GetData(State state)
+		=> new()
+		{
+			cost = upgrade != Upgrade.None ? 1 : 2,
+			exhaust = true,
+			buoyant = upgrade == Upgrade.A
+		};
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> upgrade switch
+		{
+			Upgrade.B => [
+				new AHurt
+				{
+					targetPlayer = true,
+					hurtAmount = 1
+				},
+				new AAttack
+				{
+					damage = GetDmg(s, 0),
+					weaken = true
+				},
+				new AAttack
+				{
+					damage = GetDmg(s, 2),
+					piercing = true,
+				},
+				new AStatus
+				{
+					targetPlayer = false,
+					status = ModEntry.Instance.BleedingStatus.Status,
+					statusAmount = 1
+				}
+			],
+			_ => [
+				new AHurt
+				{
+					targetPlayer = true,
+					hurtAmount = 1
+				},
+				new AAttack
+				{
+					damage = GetDmg(s, 2),
+					piercing = true,
+					weaken = true
+				},
+				new AStatus
+				{
+					targetPlayer = false,
+					status = ModEntry.Instance.BleedingStatus.Status,
+					statusAmount = 1
+				}
+			]
+		};
+}
---
+++ Dracula/Cards/ScreechCard.cs
@@ -0,0 +1,40 @@
+﻿using Nickel;
+using System.Collections.Generic;
+
+namespace Shockah.Dracula;
+
+internal sealed class ScreechCard : Card, IRegisterableCard
+{
+	public void Register(IModHelper helper)
+	{
+		helper.Content.Cards.RegisterCard("Screech", new()
+		{
+			CardType = GetType(),
+			Meta = new()
+			{
+				deck = ModEntry.Instance.DraculaDeck.Deck,
+				rarity = Rarity.rare,
+				upgradesTo = [Upgrade.A, Upgrade.B]
+			},
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Screech", "name"]).Localize
+		});
+	}
+
+	public override CardData GetData(State state)
+		=> new()
+		{
+			cost = upgrade == Upgrade.B ? 3 : 2,
+			retain = upgrade == Upgrade.A,
+			exhaust = upgrade == Upgrade.B
+		};
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> [
+			new AStatus
+			{
+				targetPlayer = false,
+				status = Status.overdrive,
+				statusAmount = upgrade == Upgrade.B ? -2 : -1
+			}
+		];
+}
