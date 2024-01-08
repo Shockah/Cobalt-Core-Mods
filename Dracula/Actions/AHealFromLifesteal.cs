9a0252ceb41e2315c8c9f139637c8df8e657b837
Michał Dolaś <me@shockah.pl>
2024-01-08 12:30:16 +0100

    Dracula: Crimson Wave

---
+++ Dracula/Actions/AHealFromLifesteal.cs
@@ -0,0 +1,29 @@
+﻿namespace Shockah.Dracula;
+
+public sealed class AHealFromLifesteal : CardAction
+{
+	public override void Begin(G g, State s, Combat c)
+	{
+		base.Begin(g, s, c);
+
+		var toHeal = c.otherShip.Get(ModEntry.Instance.LifestealStatus.Status);
+		if (toHeal <= 0)
+		{
+			timer = 0;
+			return;
+		}
+
+		c.QueueImmediate(new AHeal
+		{
+			targetPlayer = true,
+			healAmount = toHeal
+		});
+		c.QueueImmediate(new AStatus
+		{
+			targetPlayer = false,
+			mode = AStatusMode.Set,
+			status = ModEntry.Instance.LifestealStatus.Status,
+			statusAmount = 0
+		});
+	}
+}
---
+++ Dracula/Managers/LifestealManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Shockah.Dracula;
+
+internal sealed class LifestealManager : IStatusRenderHook
+{
+	public LifestealManager()
+	{
+		ModEntry.Instance.KokoroApi.RegisterStatusRenderHook(this, 0);
+	}
+
+	public bool? ShouldShowStatus(State state, Combat combat, Ship ship, Status status, int amount)
+	{
+		if (status != ModEntry.Instance.LifestealStatus.Status)
+			return null;
+		return false;
+	}
+}
