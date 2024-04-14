53093b813863257cc26a7c7b6f0587e578f88c9d
Michał Dolaś <me@shockah.pl>
2024-04-14 11:55:12 +0200

    Dyna: Extra APIs

--- Dyna/ApiImplementation.cs
+++ Dyna/ApiImplementation.cs
@@ -14,12 +14,30 @@ public sealed class ApiImplementation : IDynaApi
 	public IStatusEntry BastionStatus
 		=> BastionManager.BastionStatus;
 
+	public PDamMod FluxDamageModifier
+		=> FluxPartModManager.FluxDamageModifier;
+
 	public int GetBlastwaveDamage(Card? card, State state, int baseDamage, bool targetPlayer = false, int blastwaveIndex = 0)
 	{
 		foreach (var hook in ModEntry.Instance.HookManager.GetHooksWithProxies(ModEntry.Instance.KokoroApi, state.EnumerateAllArtifacts()))
 			baseDamage += hook.ModifyBlastwaveDamage(card, state, targetPlayer, blastwaveIndex);
 		return Math.Max(baseDamage, 0);
 	}
+
+	public bool IsBlastwave(AAttack attack)
+		=> attack.IsBlastwave();
+
+	public bool IsStunwave(AAttack attack)
+		=> attack.IsStunwave();
+
+	public int? GetBlastwaveDamage(AAttack attack)
+		=> attack.GetBlastwaveDamage();
+
+	public int GetBlastwaveRange(AAttack attack)
+		=> attack.GetBlastwaveRange();
+
+	public AAttack SetBlastwave(AAttack attack, int? damage, int range = 1, bool isStunwave = false)
+		=> attack.SetBlastwave(damage, range, isStunwave);
 
 	public void RegisterHook(IDynaHook hook, double priority)
 		=> ModEntry.Instance.HookManager.Register(hook, priority);
--- Dyna/IDynaApi.cs
+++ Dyna/IDynaApi.cs
@@ -8,7 +8,15 @@ public interface IDynaApi
 	IStatusEntry NitroStatus { get; }
 	IStatusEntry BastionStatus { get; }
 
+	PDamMod FluxDamageModifier { get; }
+
 	int GetBlastwaveDamage(Card? card, State state, int baseDamage, bool targetPlayer = false, int blastwaveIndex = 0);
+
+	bool IsBlastwave(AAttack attack);
+	bool IsStunwave(AAttack attack);
+	int? GetBlastwaveDamage(AAttack attack);
+	int GetBlastwaveRange(AAttack attack);
+	AAttack SetBlastwave(AAttack attack, int? damage, int range = 1, bool isStunwave = false);
 
 	void RegisterHook(IDynaHook hook, double priority);
 	void UnregisterHook(IDynaHook hook);
