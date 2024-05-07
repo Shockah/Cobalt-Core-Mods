293830f47a2b293c8e3ea57c6600817f9a26b79f
Michał Dolaś <me@shockah.pl>
2024-05-07 20:03:28 +0200

    CatDataExport: Card tooltips

---
+++ CatDiscordBotDataExport/Patches/CardPatches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Shockah.Shared;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Shockah.CatDiscordBotDataExport;
+
+internal sealed class CardPatches
+{
+	private static ModEntry Instance => ModEntry.Instance;
+	internal static bool ActivateAllActions = false;
+
+	public static void Apply(Harmony harmony)
+	{
+		harmony.TryPatch(
+			logger: Instance.Logger!,
+			original: () => AccessTools.DeclaredMethod(typeof(Card), nameof(Card.GetActionsOverridden)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Card_GetActionsOverridden_Postfix))
+		);
+	}
+
+	private static void Card_GetActionsOverridden_Postfix(Card __instance, ref List<CardAction> __result)
+	{
+		if (ActivateAllActions)
+			foreach (var action in __result)
+				action.disabled = false;
+	}
+}
