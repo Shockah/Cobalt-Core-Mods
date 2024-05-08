662dcd4687a61ec7ce085dab4a7d21793216265d
Michał Dolaś <me@shockah.pl>
2024-05-08 23:26:33 +0200

    MORE: Release is now flippable

--- MORE/Features/ReleaseUpgrades.cs
+++ MORE/Features/ReleaseUpgrades.cs
@@ -2,6 +2,7 @@
 using Nanoray.PluginManager;
 using Nickel;
 using Shockah.Shared;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Shockah.MORE;
@@ -15,14 +16,32 @@ internal sealed class ReleaseUpgrades : IRegisterable
 			original: () => AccessTools.DeclaredMethod(typeof(ReleaseCard), nameof(ReleaseCard.GetData)),
 			postfix: new HarmonyMethod(AccessTools.DeclaredMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ReleaseCard_GetData_Postfix)))
 		);
+		ModEntry.Instance.Harmony.TryPatch(
+			logger: ModEntry.Instance.Logger,
+			original: () => AccessTools.DeclaredMethod(typeof(ReleaseCard), nameof(ReleaseCard.GetActions)),
+			postfix: new HarmonyMethod(AccessTools.DeclaredMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ReleaseCard_GetActions_Postfix)))
+		);
 		DB.cardMetas[typeof(ReleaseCard).Name].upgradesTo = [Upgrade.A];
 	}
 
 	private static void ReleaseCard_GetData_Postfix(Card __instance, ref CardData __result)
 	{
+		__result.flippable = true;
+
 		if (__instance.upgrade != Upgrade.A)
 			return;
+
 		__result.singleUse = false;
 		__result.exhaust = true;
+	}
+
+	private static void ReleaseCard_GetActions_Postfix(Card __instance, State s, ref List<CardAction> __result)
+	{
+		if (!__instance.flipped)
+			return;
+
+		foreach (var action in __result)
+			if (action is ASpawn spawnAction)
+				spawnAction.thing.targetPlayer = !spawnAction.thing.targetPlayer;
 	}
 }
