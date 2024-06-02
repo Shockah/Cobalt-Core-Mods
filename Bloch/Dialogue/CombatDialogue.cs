45030e5b8e3e6de460aba9a0fc66e74e4f8518e2
Michał Dolaś <me@shockah.pl>
2024-06-02 20:35:29 +0200

    Bloch: Disabled combat shouts

--- Bloch/Dialogue/CombatDialogue.cs
+++ Bloch/Dialogue/CombatDialogue.cs
@@ -4,24 +4,24 @@ internal sealed class CombatDialogue
 {
 	public CombatDialogue()
 	{
-		var deck = ModEntry.Instance.BlochDeck.Deck;
-		var key = ModEntry.Instance.BlochDeck.UniqueName;
+		//var deck = ModEntry.Instance.BlochDeck.Deck;
+		//var key = ModEntry.Instance.BlochDeck.UniqueName;
 
-		DB.story.all[$"{key}::JustHit_1"] = new()
-		{
-			type = NodeType.combat,
-			allPresent = [key],
-			whoDidThat = deck,
-			playerShotJustHit = true,
-			minDamageDealtToEnemyThisAction = 1,
-			lines = [
-				new LocalizedSay()
-				{
-					who = key,
-					loopTag = "glorp",
-					Key = ["JustHit_1"],
-				}
-			]
-		};
+		//DB.story.all[$"{key}::JustHit_1"] = new()
+		//{
+		//	type = NodeType.combat,
+		//	allPresent = [key],
+		//	whoDidThat = deck,
+		//	playerShotJustHit = true,
+		//	minDamageDealtToEnemyThisAction = 1,
+		//	lines = [
+		//		new LocalizedSay()
+		//		{
+		//			who = key,
+		//			loopTag = "glorp",
+		//			Key = ["JustHit_1"],
+		//		}
+		//	]
+		//};
 	}
 }
