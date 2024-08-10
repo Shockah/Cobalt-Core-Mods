b0a1740d567642516a8d4d594689959c263999cb
Michał Dolaś <me@shockah.pl>
2024-08-10 13:44:58 +0200

    Bjorn: WIP new character

---
+++ Bjorn/Cards/SafetyProtocolCard.cs
@@ -0,0 +1,94 @@
+﻿using FSPRO;
+using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Shared;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Shockah.Bjorn;
+
+public sealed class SafetyProtocolCard : Card, IRegisterable
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Cards.RegisterCard(MethodBase.GetCurrentMethod()!.DeclaringType!.Name, new()
+		{
+			CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				deck = ModEntry.Instance.BjornDeck.Deck,
+				rarity = ModEntry.GetCardRarity(MethodBase.GetCurrentMethod()!.DeclaringType!),
+				upgradesTo = [Upgrade.A, Upgrade.B]
+			},
+			Art = helper.Content.Sprites.RegisterSpriteOrDefault(package.PackageRoot.GetRelativeFile("assets/Cards/SafetyProtocol.png"), StableSpr.cards_Shield).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "SafetyProtocol", "name"]).Localize
+		});
+	}
+
+	public override CardData GetData(State state)
+	{
+		var description = ModEntry.Instance.Localizations.Localize(["card", "SafetyProtocol", "description", upgrade.ToString(), flipped ? "energy" : "analyze"]);
+		return upgrade.Switch<CardData>(
+			none: () => new() { cost = 0, floppable = true, infinite = true, description = description },
+			a: () => new() { cost = 0, floppable = true, infinite = true, description = description },
+			b: () => new() { cost = 0, floppable = true, description = description }
+		);
+	}
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> upgrade.Switch<List<CardAction>>(
+			none: () => [new ActionCost { Analyze = !flipped, EnergyCost = flipped ? 1 : 0, Actions = [
+				new SmartShieldAction { Amount = 1 },
+			] }],
+			a: () => [new ActionCost { Analyze = !flipped, EnergyCost = flipped ? 1 : 0, Actions = [
+				new SmartShieldAction { Amount = 1 },
+				new ADrawCard { count = 1 },
+			] }],
+			b: () => [new ActionCost { Analyze = !flipped, EnergyCost = flipped ? 1 : 0, Actions = [
+				new SmartShieldAction { Amount = 2 },
+			] }]
+		);
+
+	private sealed class ActionCost : CardAction
+	{
+		public int EnergyCost;
+		public bool Analyze;
+		public required List<CardAction> Actions;
+
+		public override List<Tooltip> GetTooltips(State s)
+			=> Analyze
+				? new AnalyzeCostAction { Actions = Actions }.GetTooltips(s)
+				: Actions.SelectMany(a => a.GetTooltips(s)).ToList();
+
+		public override void Begin(G g, State s, Combat c)
+		{
+			base.Begin(g, s, c);
+
+			if (Analyze)
+			{
+				timer = 0;
+				c.QueueImmediate(new AnalyzeCostAction { Actions = Actions });
+			}
+			else if (EnergyCost > 0)
+			{
+				if (c.energy < EnergyCost)
+				{
+					timer = 0;
+					Audio.Play(Event.ZeroEnergy);
+					return;
+				}
+				else
+				{
+					c.energy -= EnergyCost;
+					c.QueueImmediate(Actions);
+				}
+			}
+			else
+			{
+				timer = 0;
+				c.QueueImmediate(Actions);
+			}
+		}
+	}
+}
---
+++ Bjorn/Features/SmartShield.cs
@@ -0,0 +1,57 @@
+﻿using Nanoray.PluginManager;
+using Nickel;
+using System;
+using System.Collections.Generic;
+
+namespace Shockah.Bjorn;
+
+internal sealed class SmartShield : IRegisterable
+{
+	internal static ISpriteEntry Icon { get; private set; } = null!;
+
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		Icon = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Icons/SmartShield.png"));
+	}
+}
+
+public sealed class SmartShieldAction : CardAction
+{
+	public bool TargetPlayer = true;
+	public required int Amount;
+
+	public override Icon? GetIcon(State s)
+		=> new() { path = SmartShield.Icon.Sprite, number = Amount, color = Colors.textMain };
+
+	public override List<Tooltip> GetTooltips(State s)
+		=> [
+			new GlossaryTooltip($"action.{ModEntry.Instance.Package.Manifest.UniqueName}::SmartShield")
+			{
+				Icon = SmartShield.Icon.Sprite,
+				TitleColor = Colors.action,
+				Title = ModEntry.Instance.Localizations.Localize(["action", "SmartShield", "name"]),
+				Description = ModEntry.Instance.Localizations.Localize(["action", "SmartShield", "description"], new { Amount = Amount }),
+			}
+		];
+
+	public override void Begin(G g, State s, Combat c)
+	{
+		base.Begin(g, s, c);
+		timer = 0;
+
+		var ship = TargetPlayer ? s.ship : c.otherShip;
+		var shieldThatFits = ship.GetMaxShield() - ship.Get(Status.shield);
+
+		var shieldToAdd = Math.Min(shieldThatFits, Amount);
+		var tempShieldToAdd = Amount - shieldToAdd;
+
+		List<CardAction> actions = [];
+
+		if (shieldToAdd > 0)
+			actions.Add(new AStatus { targetPlayer = TargetPlayer, status = Status.shield, statusAmount = shieldToAdd });
+		if (tempShieldToAdd > 0)
+			actions.Add(new AStatus { targetPlayer = TargetPlayer, status = Status.tempShield, statusAmount = tempShieldToAdd });
+
+		c.QueueImmediate(actions);
+	}
+}
