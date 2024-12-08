52ff5dc72b8b0f450f18dcf88b7dafca01ff8382
Michał Dolaś <me@shockah.pl>
2024-12-08 19:01:10 +0100

    Bjorn: Accelerate card

---
+++ Bjorn/Cards/AccelerateCard.cs
@@ -0,0 +1,60 @@
+﻿using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Shared;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Shockah.Bjorn;
+
+public sealed class AccelerateCard : Card, IRegisterable
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Cards.RegisterCard(MethodBase.GetCurrentMethod()!.DeclaringType!.Name, new()
+		{
+			CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				deck = ModEntry.Instance.BjornDeck.Deck,
+				rarity = ModEntry.GetCardRarity(MethodBase.GetCurrentMethod()!.DeclaringType!),
+				upgradesTo = [Upgrade.A, Upgrade.B],
+			},
+			Art = helper.Content.Sprites.RegisterSpriteOrDefault(package.PackageRoot.GetRelativeFile("assets/Cards/Accelerate.png"), StableSpr.cards_dizzy).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Accelerate", "name"]).Localize,
+		});
+	}
+
+	public override CardData GetData(State state)
+	{
+		var description = ModEntry.Instance.Localizations.Localize(["card", "Accelerate", "description", upgrade.ToString()]);
+		return upgrade.Switch<CardData>(
+			() => new() { cost = 1, exhaust = true, description = description },
+			() => new() { cost = 1, description = description },
+			() => new() { cost = 3, exhaust = true, description = description }
+		);
+	}
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> upgrade.Switch<List<CardAction>>(
+			none: () => [new AnalyzeCostAction { FilterAccelerated = false, FilterMinCost = 2, FilterExhaust = false, Action = new Action { Permanent = false } }],
+			a: () => [new AnalyzeCostAction { FilterAccelerated = false, FilterMinCost = 2, FilterExhaust = false, Action = new Action { Permanent = false } }],
+			b: () => [new AnalyzeCostAction { Permanent = true, FilterAccelerated = false, FilterMinCost = 2, FilterExhaust = false, Action = new Action { Permanent = true } }]
+		);
+
+	private sealed class Action : CardAction
+	{
+		public required bool Permanent;
+
+		public override List<Tooltip> GetTooltips(State s)
+			=> [.. AcceleratedManager.Trait.Configuration.Tooltips?.Invoke(s, null) ?? []];
+
+		public override void Begin(G g, State s, Combat c)
+		{
+			base.Begin(g, s, c);
+			if (selectedCard is null)
+				return;
+			
+			ModEntry.Instance.Helper.Content.Cards.SetCardTraitOverride(s, selectedCard, AcceleratedManager.Trait, true, permanent: Permanent);
+		}
+	}
+}
---
+++ Bjorn/Features/Accelerated.cs
@@ -0,0 +1,83 @@
+﻿using HarmonyLib;
+using Nanoray.PluginManager;
+using Nickel;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Shockah.Bjorn;
+
+internal static class AcceleratedCardSelectFiltersExt
+{
+	public static ACardSelect SetFilterAccelerated(this ACardSelect self, bool? value)
+	{
+		ModEntry.Instance.Helper.ModData.SetOptionalModData(self, "FilterAccelerated", value);
+		return self;
+	}
+}
+
+internal sealed class AcceleratedManager : IRegisterable
+{
+	internal static ISpriteEntry Icon { get; private set; } = null!;
+
+	internal static ICardTraitEntry Trait { get; private set; } = null!;
+
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		Icon = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Icons/Accelerated.png"));
+
+		Trait = helper.Content.Cards.RegisterTrait("Accelerated", new()
+		{
+			Icon = (_, _) => Icon.Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["cardTrait", "Accelerated", "name"]).Localize,
+			Tooltips = (_, _) => [
+				new GlossaryTooltip($"cardtrait.{package.Manifest.UniqueName}::Accelerated")
+				{
+					Icon = Icon.Sprite,
+					TitleColor = Colors.cardtrait,
+					Title = ModEntry.Instance.Localizations.Localize(["cardTrait", "Accelerated", "name"]),
+					Description = ModEntry.Instance.Localizations.Localize(["cardTrait", "Accelerated", "description"]),
+				}
+			]
+		});
+
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(Card), nameof(Card.GetCurrentCost)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Card_GetCurrentCost_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(ACardSelect), nameof(ACardSelect.BeginWithRoute)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ACardSelect_BeginWithRoute_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(CardBrowse), nameof(CardBrowse.GetCardList)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(CardBrowse_GetCardList_Postfix))
+		);
+	}
+
+	private static void Card_GetCurrentCost_Postfix(Card __instance, State s, ref int __result)
+	{
+		if (!ModEntry.Instance.Helper.Content.Cards.IsCardTraitActive(s, __instance, Trait))
+			return;
+		__result = Math.Max(__result - 1, 0);
+	}
+	
+	private static void ACardSelect_BeginWithRoute_Postfix(ACardSelect __instance, ref Route? __result)
+	{
+		if (__result is not CardBrowse route)
+			return;
+		
+		ModEntry.Instance.Helper.ModData.SetOptionalModData(route, "FilterAccelerated", ModEntry.Instance.Helper.ModData.GetOptionalModData<bool>(__instance, "FilterAccelerated"));
+	}
+
+	private static void CardBrowse_GetCardList_Postfix(CardBrowse __instance, G g, ref List<Card> __result)
+	{
+		var filterAccelerated = ModEntry.Instance.Helper.ModData.GetOptionalModData<bool>(__instance, "FilterAccelerated");
+		if (filterAccelerated is null)
+			return;
+
+		for (var i = __result.Count - 1; i >= 0; i--)
+			if (filterAccelerated is not null && ModEntry.Instance.Helper.Content.Cards.IsCardTraitActive(g.state, __result[i], Trait) != filterAccelerated.Value)
+				__result.RemoveAt(i);
+	}
+}
