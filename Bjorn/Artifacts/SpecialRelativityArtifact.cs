29f82d8ff7014d47b4b29d7423eb9f543d3b4006
Michał Dolaś <me@shockah.pl>
2024-12-15 18:18:16 +0100

    Bjorn: More artifacts

---
+++ Bjorn/Artifacts/SpecialRelativityArtifact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.Bjorn;
+
+internal sealed class SpecialRelativityArtifact : Artifact, IRegisterable, IBjornApi.IHook
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Artifacts.RegisterArtifact("SpecialRelativity", new()
+		{
+			ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				owner = ModEntry.Instance.BjornDeck.Deck,
+				pools = ModEntry.GetArtifactPools(MethodBase.GetCurrentMethod()!.DeclaringType!)
+			},
+			Sprite = helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Artifacts/SpecialRelativity.png")).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "SpecialRelativity", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "SpecialRelativity", "description"]).Localize
+		});
+	}
+
+	public override List<Tooltip> GetExtraTooltips()
+		=> StatusMeta.GetTooltips(RelativityManager.RelativityStatus.Status, 1);
+
+	public void ModifyRelativityLimit(IBjornApi.IHook.IModifyRelativityLimitArgs args)
+		=> args.Limit++;
+}
