2e5e050743079d8785ce32d07cc9a83d7df4c437
Michał Dolaś <me@shockah.pl>
2024-12-16 10:14:35 +0100

    Bjorn: Fixes

--- Bjorn/Cards/SafetyProtocolCard.cs
+++ Bjorn/Cards/SafetyProtocolCard.cs
@@ -1,10 +1,10 @@
-﻿using Nanoray.PluginManager;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Nanoray.PluginManager;
 using Nickel;
 using Shockah.Kokoro;
 using Shockah.Shared;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace Shockah.Bjorn;
 
@@ -26,29 +26,37 @@ public sealed class SafetyProtocolCard : Card, IRegisterable
 			Art = helper.Content.Sprites.RegisterSpriteOrDefault(package.PackageRoot.GetRelativeFile("assets/Cards/SafetyProtocol.png"), StableSpr.cards_Shield).Sprite,
 			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "SafetyProtocol", "name"]).Localize,
 		});
+
+		helper.Content.Cards.OnGetDynamicInnateCardTraitOverrides += (_, e) =>
+		{
+			if (e.Card is not SafetyProtocolCard card)
+				return;
+
+			card.GetDataReentry++;
+			try
+			{
+				var unplayable = card.GetDataReentry == 1 && e.State.route is Combat { routeOverride: null } combat && (
+					card.flipped
+						? combat.energy < card.GetCurrentCost(MG.inst.g.state) + 1
+						: combat.hand.All(someCard => !someCard.IsAnalyzable(MG.inst.g.state, combat))
+				);
+				
+				if (unplayable)
+					e.SetOverride(ModEntry.Instance.Helper.Content.Cards.UnplayableCardTrait, true);
+			}
+			finally
+			{
+				card.GetDataReentry--;
+			}
+		};
 	}
 
 	public override CardData GetData(State state)
-	{
-		GetDataReentry++;
-		try
-		{
-			var unplayable = GetDataReentry == 1 && state.route is Combat combat && (
-				flipped
-					? combat.energy < GetCurrentCost(state)
-					: combat.hand.All(card => !card.IsAnalyzable(state, combat))
-			);
-			return upgrade.Switch<CardData>(
-				none: () => new() { cost = 0, floppable = true, infinite = true, unplayable = unplayable },
-				a: () => new() { cost = 0, floppable = true, infinite = true, unplayable = unplayable },
-				b: () => new() { cost = 0, floppable = true, unplayable = unplayable }
-			);
-		}
-		finally
-		{
-			GetDataReentry--;
-		}
-	}
+		=> upgrade.Switch<CardData>(
+			none: () => new() { cost = 0, floppable = true, infinite = true },
+			a: () => new() { cost = 0, floppable = true, infinite = true },
+			b: () => new() { cost = 0, floppable = true }
+		);
 
 	public override List<CardAction> GetActions(State s, Combat c)
 		=> upgrade.Switch<List<CardAction>>(
