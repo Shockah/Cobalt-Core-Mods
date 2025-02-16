43c03cccdb1c79362c6dec3615c3eb2c7bb2180b
Michał Dolaś <me@shockah.pl>
2025-02-16 22:53:06 +0100

    Bjorn: Side Projects artifact

--- Bjorn/ModEntry.cs
+++ Bjorn/ModEntry.cs
@@ -64,6 +64,7 @@ public sealed class ModEntry : SimpleMod
 	private static readonly IReadOnlyList<Type> CommonArtifacts = [
 		typeof(FourDChessArtifact),
 		typeof(RelativityTheoryArtifact),
+		typeof(SideProjectsArtifact),
 		typeof(SpecialRelativityArtifact),
 	];
 
