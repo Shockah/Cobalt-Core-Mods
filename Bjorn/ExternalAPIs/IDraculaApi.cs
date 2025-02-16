6a841d545a15d6726391c6e627ed79556abfb14b
Michał Dolaś <me@shockah.pl>
2025-02-16 14:44:03 +0100

    Bjorn: Dracula integration

---
+++ Bjorn/ExternalAPIs/IDraculaApi.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shockah.Dracula;
+
+public interface IDraculaApi
+{
+	void RegisterBloodTapOptionProvider(Status status, Func<State, Combat, Status, List<CardAction>> provider);
+}
