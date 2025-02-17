1999ddc2a90ae4d520ecd72cd22f4ea43fc1096e
Michał Dolaś <me@shockah.pl>
2025-02-17 23:38:51 +0100

    Bjorn: Overtime artifact

---
+++ Bjorn/Artifacts/OvertimeArtifact.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.Bjorn;
+
+internal sealed class OvertimeArtifact : Artifact, IRegisterable, IBjornApi.IHook
+{
+	public const int Threshold = 5;
+	
+	public int AnalyzedCards;
+	
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Artifacts.RegisterArtifact("Overtime", new()
+		{
+			ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				owner = ModEntry.Instance.BjornDeck.Deck,
+				pools = ModEntry.GetArtifactPools(MethodBase.GetCurrentMethod()!.DeclaringType!)
+			},
+			Sprite = helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Artifacts/Overtime.png")).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "Overtime", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["artifact", "Overtime", "description"]).Localize
+		});
+	}
+
+	public override int? GetDisplayNumber(State s)
+		=> AnalyzedCards;
+
+	public override List<Tooltip> GetExtraTooltips()
+		=> AnalyzeManager.GetAnalyzeTooltips(MG.inst.g?.state ?? DB.fakeState);
+
+	public override void OnCombatStart(State state, Combat combat)
+	{
+		base.OnCombatStart(state, combat);
+		AnalyzedCards = 0;
+	}
+
+	public void OnCardsAnalyzed(IBjornApi.IHook.IOnCardsAnalyzedArgs args)
+	{
+		foreach (var card in args.Cards)
+		{
+			AnalyzedCards++;
+			if (AnalyzedCards < Threshold)
+				continue;
+
+			AnalyzedCards -= Threshold;
+			ModEntry.Instance.Helper.Content.Cards.SetCardTraitOverride(args.State, card, AnalyzeManager.AnalyzedTrait, false, false);
+			if (args.Permanent)
+				ModEntry.Instance.Helper.Content.Cards.SetCardTraitOverride(args.State, card, AnalyzeManager.AnalyzedTrait, false, true);
+			Pulse();
+		}
+	}
+}
