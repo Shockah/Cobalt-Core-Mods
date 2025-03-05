9fa62b932362c8227bbe57c65bb4aacee91a2500
Michał Dolaś <me@shockah.pl>
2025-03-05 13:22:38 +0100

    Natasha: CAT duo artifact

--- Natasha/ModEntry.cs
+++ Natasha/ModEntry.cs
@@ -87,6 +87,7 @@ public sealed class ModEntry : SimpleMod
 	];
 
 	internal static readonly IReadOnlyList<Type> DuoArtifacts = [
+		typeof(NatashaCatArtifact),
 		typeof(NatashaDizzyArtifact),
 		typeof(NatashaIsaacArtifact),
 		typeof(NatashaPeriArtifact),
