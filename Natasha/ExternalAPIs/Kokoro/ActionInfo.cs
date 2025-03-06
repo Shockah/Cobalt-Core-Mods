8a64d6b1a4b9e608a0fb3d4aaddc4729661b0af7
Michał Dolaś <me@shockah.pl>
2025-03-06 14:12:07 +0100

    Natasha: Fixed Kokoro dependency

--- Natasha/ExternalAPIs/Kokoro/ActionInfo.cs
+++ Natasha/ExternalAPIs/Kokoro/ActionInfo.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Shockah.Kokoro;
+﻿namespace Shockah.Kokoro;
 
 public partial interface IKokoroApi
 {
@@ -43,74 +41,6 @@ public partial interface IKokoroApi
 			/// <param name="state">The game state.</param>
 			/// <param name="source">The card the given action came from.</param>
 			void SetSourceCard(CardAction action, State state, Card? source);
-
-			/// <summary>
-			/// Returns the ID of the interaction the given action came from.
-			/// All actions of a single <see cref="Card.GetActionsOverridden"/> call will have the same ID.
-			/// </summary>
-			/// <param name="action">The action.</param>
-			/// <returns>The ID of the interaction.</returns>
-			Guid? GetInteractionId(CardAction action);
-			
-			/// <summary>
-			/// Sets the ID of the interaction the given action came from.
-			/// All actions of a single <see cref="Card.GetActionsOverridden"/> call should have the same ID.
-			/// </summary>
-			/// <param name="action">The action.</param>
-			/// <param name="interactionId">The ID of the interaction.</param>
-			void SetInteractionId(CardAction action, Guid? interactionId);
-			
-			/// <summary>
-			/// Registers a new hook related to action costs.
-			/// </summary>
-			/// <param name="hook">The hook.</param>
-			/// <param name="priority">The priority for the hook. Higher priority hooks are called before lower priority ones. Defaults to <c>0</c>.</param>
-			void RegisterHook(IHook hook, double priority = 0);
-			
-			/// <summary>
-			/// Unregisters the given hook related to action costs.
-			/// </summary>
-			/// <param name="hook">The hook.</param>
-			void UnregisterHook(IHook hook);
-			
-			/// <summary>
-			/// A hook related to action information.
-			/// </summary>
-			public interface IHook : IKokoroV2ApiHook
-			{
-				/// <summary>
-				/// Called when a user interaction has ended (for example, all actions of a card were executed).
-				/// </summary>
-				/// <param name="args">The arguments for the hook method.</param>
-				void OnInteractionEnded(IOnInteractionEndedArgs args) { }
-				
-				/// <summary>
-				/// The arguments for the <see cref="OnInteractionEnded"/> hook method.
-				/// </summary>
-				public interface IOnInteractionEndedArgs
-				{
-					/// <summary>
-					/// The game state.
-					/// </summary>
-					State State { get; }
-					
-					/// <summary>
-					/// The current combat.
-					/// </summary>
-					Combat Combat { get; }
-				
-					/// <summary>
-					/// The card this interaction was for, if any.
-					/// </summary>
-					Card? Card { get; }
-					
-					/// <summary>
-					/// The ID of the interaction.
-					/// All actions of a single <see cref="Card.GetActionsOverridden"/> call will have the same ID.
-					/// </summary>
-					Guid InteractionId { get; }
-				}
-			}
 		}
 	}
 }
