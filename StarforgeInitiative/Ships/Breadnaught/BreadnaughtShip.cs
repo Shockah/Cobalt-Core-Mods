d211a1b076965dda9c4e1886dfaea430d711f381
Michał Dolaś <me@shockah.pl>
2025-03-09 22:23:15 +0100

    StarforgeInitiative: The Breadnaught WIP

---
+++ StarforgeInitiative/Ships/Breadnaught/BreadnaughtShip.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.StarforgeInitiative;
+
+internal sealed class BreadnaughtShip : IRegisterable
+{
+	private static readonly List<Type> ArtifactTypes = [
+		typeof(BreadnaughtMinigunArtifact),
+	];
+	
+	private static readonly List<Type> CardTypes = [
+	];
+	
+	private static readonly List<Type> RegisterableTypes = [
+		.. ArtifactTypes,
+		.. CardTypes,
+		typeof(BarrelSpinManager),
+	];
+	
+	internal static IShipEntry ShipEntry { get; private set; } = null!;
+	
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		ShipEntry = helper.Content.Ships.RegisterShip("Breadnaught", new()
+		{
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Breadnaught", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Breadnaught", "description"]).Localize,
+			Ship = new()
+			{
+				ship = new()
+				{
+					hull = 12,
+					hullMax = 12,
+					shieldMaxBase = 4,
+					parts = [
+						new() { type = PType.missiles, skin = "missiles_conveyor" },
+						new() { type = PType.wing, skin = "wing_junker", flip = true },
+						new() { type = PType.cannon, skin = "cannon_conveyor" },
+						new() { type = PType.wing, skin = "wing_junker" },
+						new() { type = PType.cockpit, skin = "cockpit_artemis" },
+					]
+				},
+				artifacts = [
+					new ShieldPrep(),
+					new BreadnaughtMinigunArtifact(),
+				],
+				cards = [
+					new CannonColorless(),
+					new CannonColorless(),
+					new DodgeColorless(),
+					new BasicShieldColorless(),
+				],
+			},
+			UnderChassisSprite = StableSpr.parts_chassis,
+			ExclusiveArtifactTypes = ArtifactTypes.ToHashSet(),
+		});
+		
+		foreach (var type in RegisterableTypes)
+			AccessTools.DeclaredMethod(type, nameof(IRegisterable.Register))?.Invoke(null, [package, helper]);
+	}
+}
