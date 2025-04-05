8d572ad80bff92ad1f403d46288dc0df5b0ed848
Michał Dolaś <me@shockah.pl>
2025-04-05 13:27:38 +0200

    CustomRunOptions: WIP new mod

---
+++ CustomRunOptions/IRegisterable.cs
@@ -0,0 +1,9 @@
+﻿using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.CustomRunOptions;
+
+internal interface IRegisterable
+{
+	static virtual void Register(IPluginPackage<IModManifest> package, IModHelper helper) { }
+}
---
+++ CustomRunOptions/ModEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.CustomRunOptions;
+
+internal sealed class ModEntry : SimpleMod
+{
+	internal static ModEntry Instance { get; private set; } = null!;
+	internal IHarmony Harmony { get; }
+
+	internal readonly ILocalizationProvider<IReadOnlyList<string>> AnyLocalizations;
+	internal readonly ILocaleBoundNonNullLocalizationProvider<IReadOnlyList<string>> Localizations;
+	
+	private static IReadOnlyList<Type> CustomRunOptionTypes { get; } = [
+		typeof(DailyModifiersCustomRunOption),
+	];
+	
+	private static IReadOnlyList<Type> RegisterableTypes { get; } = [
+		typeof(NewRunOptionsButton),
+	];
+
+	internal static IReadOnlyList<ICustomRunOption> CustomRunOptions = CustomRunOptionTypes
+		.Select(t => (ICustomRunOption)Activator.CreateInstance(t)!)
+		.ToList();
+	
+	public ModEntry(IPluginPackage<IModManifest> package, IModHelper helper, ILogger logger) : base(package, helper, logger)
+	{
+		Instance = this;
+		Harmony = helper.Utilities.Harmony;
+
+		this.AnyLocalizations = new JsonLocalizationProvider(
+			tokenExtractor: new SimpleLocalizationTokenExtractor(),
+			localeStreamFunction: locale => package.PackageRoot.GetRelativeFile($"i18n/{locale}.json").OpenRead()
+		);
+		this.Localizations = new MissingPlaceholderLocalizationProvider<IReadOnlyList<string>>(
+			new CurrentLocaleOrEnglishLocalizationProvider<IReadOnlyList<string>>(this.AnyLocalizations)
+		);
+
+		foreach (var type in RegisterableTypes)
+			AccessTools.DeclaredMethod(type, nameof(IRegisterable.Register))?.Invoke(null, [package, helper]);
+	}
+}
