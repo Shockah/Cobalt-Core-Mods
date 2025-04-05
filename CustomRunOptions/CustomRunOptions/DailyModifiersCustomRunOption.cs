3b1014018b3ce73c1ba095a781502e44d2f474e2
Michał Dolaś <me@shockah.pl>
2025-04-05 13:27:38 +0200

    CustomRunOptions: WIP new mod

---
+++ CustomRunOptions/CustomRunOptions/DailyModifiersCustomRunOption.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Nanoray.PluginManager;
+using Nickel;
+using Nickel.ModSettings;
+
+namespace Shockah.CustomRunOptions;
+
+internal sealed class DailyModifiersCustomRunOption : ICustomRunOption
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public IReadOnlyList<Vec> RenderInNewRunOptions(G g, Vec centerLinePosition, RunConfig runConfig)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public IModSettingsApi.IModSetting MakeCustomRunSettings(IPluginPackage<IModManifest> package, IModSettingsApi api, RunConfig runConfig)
+	{
+		throw new System.NotImplementedException();
+	}
+}
