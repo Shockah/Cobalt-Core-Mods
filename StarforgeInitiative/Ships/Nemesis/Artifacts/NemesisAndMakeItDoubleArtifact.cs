823b175636cd6bc44626499794a1e1a6cc7eb587
Michał Dolaś <me@shockah.pl>
2025-05-08 09:53:54 +0200

    StarforgeInitiative: WIP Nemesis ship

---
+++ StarforgeInitiative/Ships/Nemesis/Artifacts/NemesisAndMakeItDoubleArtifact.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using System.Reflection;
+using FSPRO;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.StarforgeInitiative;
+
+internal sealed class NemesisAndMakeItDoubleArtifact : Artifact, IRegisterable
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Artifacts.RegisterArtifact("NemesisAndMakeItDouble", new()
+		{
+			ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				owner = Deck.colorless,
+				pools = [ArtifactPool.EventOnly],
+				unremovable = true,
+			},
+			Sprite = helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Nemesis/Artifact/AndMakeItDouble.png")).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Nemesis", "artifact", "AndMakeItDouble", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Nemesis", "artifact", "AndMakeItDouble", "description"]).Localize
+		});
+	}
+
+	public override void OnTurnEnd(State state, Combat combat)
+	{
+		base.OnTurnEnd(state, combat);
+		combat.QueueImmediate(new ActivateCannonsAction());
+	}
+
+	public override void OnTurnStart(State state, Combat combat)
+	{
+		base.OnTurnStart(state, combat);
+		combat.QueueImmediate(new DeactivateCannonsAction());
+	}
+
+	private sealed class DeactivateCannonsAction : CardAction
+	{
+		public override void Begin(G g, State s, Combat c)
+		{
+			base.Begin(g, s, c);
+			timer = 0.3;
+			
+			if (s.ship.key != NemesisShip.ShipEntry.UniqueName)
+			{
+				timer = 0;
+				return;
+			}
+
+			var parts = s.ship.parts
+				.Where(p => p.type == PType.cannon)
+				.Where(p => p.skin == "wing_ares")
+				.ToList();
+
+			foreach (var part in parts)
+				part.active = false;
+			if (parts.Count != 0)
+				parts[0].active = true;
+			Audio.Play(Event.TogglePart);
+		}
+	}
+
+	private sealed class ActivateCannonsAction : CardAction
+	{
+		public override void Begin(G g, State s, Combat c)
+		{
+			base.Begin(g, s, c);
+			timer = 0.3;
+
+			if (s.ship.key != NemesisShip.ShipEntry.UniqueName)
+			{
+				timer = 0;
+				return;
+			}
+
+			var parts = s.ship.parts
+				.Where(p => p.type == PType.cannon)
+				.Where(p => p.skin == "wing_ares")
+				.ToList();
+
+			foreach (var part in parts)
+				part.active = true;
+			Audio.Play(Event.TogglePart);
+		}
+	}
+}
---
+++ StarforgeInitiative/Ships/Nemesis/Artifacts/NemesisPrepareForTroubleArtifact.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.StarforgeInitiative;
+
+internal sealed class NemesisPrepareForTroubleArtifact : Artifact, IRegisterable
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Artifacts.RegisterArtifact("NemesisPrepareForTrouble", new()
+		{
+			ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				owner = Deck.colorless,
+				pools = [ArtifactPool.EventOnly],
+				unremovable = true,
+			},
+			Sprite = helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Nemesis/Artifact/PrepareForTrouble.png")).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Nemesis", "artifact", "PrepareForTrouble", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Nemesis", "artifact", "PrepareForTrouble", "description"]).Localize
+		});
+	}
+
+	public override List<Tooltip> GetExtraTooltips()
+		=> [
+			.. StatusMeta.GetTooltips(Status.tempPayback, 1),
+			.. StatusMeta.GetTooltips(Status.payback, 1),
+		];
+
+	public override void OnTurnEnd(State state, Combat combat)
+	{
+		base.OnTurnEnd(state, combat);
+		var amount = combat.otherShip.Get(Status.payback) + combat.otherShip.Get(Status.tempPayback);
+		combat.QueueImmediate(new AStatus { targetPlayer = true, status = Status.tempPayback, statusAmount = amount + 1, artifactPulse = Key() });
+	}
+}
