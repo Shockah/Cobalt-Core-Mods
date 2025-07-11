a135250e1e70dd8cc86f11f597b45b9d0144265a
Michał Dolaś <me@shockah.pl>
2025-07-11 19:06:31 +0200

    Gary: New WIP character

---
+++ Gary/Features/Pack.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using HarmonyLib;
+using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nanoray.Shrike;
+using Nanoray.Shrike.Harmony;
+using Nickel;
+
+namespace Shockah.Gary;
+
+internal sealed class PackManager : IRegisterable
+{
+	internal static IStatusEntry PackStatus { get; private set; } = null!;
+	internal static IStatusEntry CramStatus { get; private set; } = null!;
+
+	private static StuffBase? ObjectBeingPackedInto;
+	
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		PackStatus = ModEntry.Instance.Helper.Content.Statuses.RegisterStatus("Pack", new()
+		{
+			Definition = new()
+			{
+				icon = ModEntry.Instance.Helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Status/Pack.png")).Sprite,
+				color = new("23EEB6"),
+				isGood = true,
+			},
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["status", "Pack", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["status", "Pack", "description"]).Localize
+		});
+		
+		CramStatus = ModEntry.Instance.Helper.Content.Statuses.RegisterStatus("Cram", new()
+		{
+			Definition = new()
+			{
+				icon = ModEntry.Instance.Helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Status/Cram.png")).Sprite,
+				color = new("23EEB6"),
+				isGood = true,
+			},
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["status", "Cram", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["status", "Cram", "description"]).Localize
+		});
+		
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(ASpawn), nameof(ASpawn.Begin)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ASpawn_Begin_Prefix)),
+			finalizer: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ASpawn_Begin_Finalizer))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(Combat), nameof(Combat.DestroyDroneAt)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Combat_DestroyDroneAt_Prefix)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Combat_DestroyDroneAt_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(ADroneTurn), nameof(ADroneTurn.Begin)),
+			transpiler: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ADroneTurn_Begin_Transpiler))
+		);
+	}
+
+	internal static List<StuffBase>? GetPackedObjects(StuffBase @object)
+		=> ModEntry.Instance.Helper.ModData.GetOptionalModData<List<StuffBase>>(@object, "PackedObjects");
+
+	internal static void PushPackedObject(Combat combat, int worldX, StuffBase pushed)
+	{
+		ref var @object = ref CollectionsMarshal.GetValueRefOrAddDefault(combat.stuff, worldX, out var objectExists);
+		if (!objectExists)
+		{
+			@object = pushed;
+			return;
+		}
+
+		List<StuffBase> packedObjects = [
+			.. GetPackedObjects(pushed) ?? [],
+			@object!,
+			.. GetPackedObjects(@object!) ?? [],
+		];
+		ModEntry.Instance.Helper.ModData.RemoveModData(@object!, "PackedObjects");
+		ModEntry.Instance.Helper.ModData.SetModData(pushed, "PackedObjects", packedObjects);
+		@object = pushed;
+	}
+
+	internal static bool PopPackedObject(Combat combat, int worldX, bool removeLast)
+	{
+		if (!combat.stuff.Remove(worldX, out var @object))
+			return false;
+		if (GetPackedObjects(@object) is not { } packedObjects || packedObjects.Count == 0)
+		{
+			if (!removeLast)
+				combat.stuff[worldX] = @object;
+			return removeLast;
+		}
+
+		ModEntry.Instance.Helper.ModData.RemoveModData(@object, "PackedObjects");
+		@object = packedObjects[^1];
+
+		packedObjects = packedObjects.Count == 0 ? null : packedObjects.Take(packedObjects.Count - 1).ToList();
+		ModEntry.Instance.Helper.ModData.SetOptionalModData(@object, "PackedObjects", packedObjects);
+		combat.stuff[worldX] = @object;
+		return true;
+	}
+	
+	private static void ASpawn_Begin_Prefix(ASpawn __instance, State s, Combat c, bool __runOriginal)
+	{
+		if (!__runOriginal)
+			return;
+		
+		var ship = __instance.fromPlayer ? s.ship : c.otherShip;
+		if (ship.GetPartTypeCount(PType.missiles) > 1 && !__instance.multiBayVolley)
+			return;
+		
+		var packAmount = ship.Get(PackStatus.Status);
+		var cramAmount = ship.Get(CramStatus.Status);
+		if (packAmount <= 0)
+			return;
+
+		var worldX = __instance.GetWorldX(s, c);
+		if (!c.stuff.TryGetValue(worldX, out var existingThing))
+			return;
+
+		var packSize = 1 + (GetPackedObjects(existingThing)?.Count ?? 0);
+		if (packSize > packAmount + cramAmount)
+			return;
+		
+		var packToRemove = packSize - cramAmount;
+		ship.Add(PackStatus.Status, -packToRemove);
+
+		ObjectBeingPackedInto = existingThing;
+		c.stuff.Remove(worldX);
+	}
+
+	private static void ASpawn_Begin_Finalizer(ASpawn __instance, State s, Combat c)
+	{
+		if (ObjectBeingPackedInto is null)
+			return;
+		
+		var ship = __instance.fromPlayer ? s.ship : c.otherShip;
+		if (ship.GetPartTypeCount(PType.missiles) > 1 && !__instance.multiBayVolley)
+			return;
+		
+		var worldX = __instance.GetWorldX(s, c);
+		var existingObject = c.stuff.GetValueOrDefault(worldX);
+
+		c.stuff.Remove(worldX);
+		PushPackedObject(c, worldX, ObjectBeingPackedInto);
+		if (existingObject is not null)
+			PushPackedObject(c, worldX, existingObject);
+		
+		ObjectBeingPackedInto = null;
+	}
+
+	private static void Combat_DestroyDroneAt_Prefix(Combat __instance, int x, out StuffBase? __state)
+		=> __state = __instance.stuff.GetValueOrDefault(x);
+
+	private static void Combat_DestroyDroneAt_Postfix(Combat __instance, int x, in StuffBase? __state)
+	{
+		if (__state is null)
+			return;
+
+		if (__instance.stuff.Remove(x, out var existingThing))
+		{
+			PushPackedObject(__instance, x, __state);
+			PushPackedObject(__instance, x, existingThing);
+			return;
+		}
+
+		if (GetPackedObjects(__state) is not { } packedObjects || packedObjects.Count == 0)
+			return;
+		
+		ModEntry.Instance.Helper.ModData.RemoveModData(__state, "PackedObjects");
+		
+		var newObject = packedObjects[^1];
+		packedObjects = packedObjects.Count == 0 ? null : packedObjects.Take(packedObjects.Count - 1).ToList();
+		ModEntry.Instance.Helper.ModData.SetOptionalModData(newObject, "PackedObjects", packedObjects);
+		
+		PushPackedObject(__instance, x, newObject);
+	}
+	
+	[SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+	private static IEnumerable<CodeInstruction> ADroneTurn_Begin_Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase originalMethod)
+	{
+		try
+		{
+			return new SequenceBlockMatcher<CodeInstruction>(instructions)
+				.Find([
+					ILMatches.Ldarg(3),
+					ILMatches.Ldfld("stuff"),
+					ILMatches.Ldloc<int>(originalMethod).GetLocalIndex(out var worldXLocalIndex),
+					ILMatches.Call("get_Item"),
+					ILMatches.Ldarg(2),
+					ILMatches.Ldarg(3),
+					ILMatches.Call("GetActions"),
+				])
+				.Insert(SequenceMatcherPastBoundsDirection.After, SequenceMatcherInsertionResultingBounds.IncludingInsertion, [
+					new CodeInstruction(OpCodes.Ldarg_2),
+					new CodeInstruction(OpCodes.Ldarg_3),
+					new CodeInstruction(OpCodes.Ldloc, worldXLocalIndex.Value),
+					new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(ADroneTurn_Begin_Transpiler_ModifyActions))),
+				])
+				.AllElements();
+		}
+		catch (Exception ex)
+		{
+			ModEntry.Instance.Logger.LogError("Could not patch method {Method} - {Mod} probably won't work.\nReason: {Exception}", originalMethod, ModEntry.Instance.Package.Manifest.UniqueName, ex);
+			return instructions;
+		}
+	}
+
+	private static List<CardAction> ADroneTurn_Begin_Transpiler_ModifyActions(List<CardAction> actions, State state, Combat combat, int worldX)
+	{
+		if (!combat.stuff.TryGetValue(worldX, out var existingThing))
+			return actions;
+		if (GetPackedObjects(existingThing) is not { } packedObjects || packedObjects.Count == 0)
+			return actions;
+		
+		actions.InsertRange(0, packedObjects.SelectMany(packedObject => packedObject.GetActions(state, combat) ?? []));
+		return actions;
+	}
+}
---
+++ Gary/ModEntry.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Shared;
+
+namespace Shockah.Gary;
+
+public sealed class ModEntry : SimpleMod
+{
+	internal static ModEntry Instance { get; private set; } = null!;
+	internal readonly IHarmony Harmony;
+	// internal readonly IKokoroApi.IV2 KokoroApi;
+	internal readonly ILocalizationProvider<IReadOnlyList<string>> AnyLocalizations;
+	internal readonly ILocaleBoundNonNullLocalizationProvider<IReadOnlyList<string>> Localizations;
+
+	internal IDeckEntry BjornDeck { get; }
+
+	private static readonly IReadOnlyList<Type> CommonCardTypes = [
+	];
+
+	private static readonly IReadOnlyList<Type> UncommonCardTypes = [
+	];
+
+	private static readonly IReadOnlyList<Type> RareCardTypes = [
+	];
+
+	private static readonly IEnumerable<Type> AllCardTypes
+		= [
+			.. CommonCardTypes,
+			.. UncommonCardTypes,
+			.. RareCardTypes,
+			//typeof(BlochExeCard),
+		];
+
+	private static readonly IReadOnlyList<Type> CommonArtifacts = [
+	];
+
+	private static readonly IReadOnlyList<Type> BossArtifacts = [
+	];
+
+	private static readonly IReadOnlyList<Type> DuoArtifacts = [
+	];
+
+	private static readonly IEnumerable<Type> AllArtifactTypes
+		= [
+			.. CommonArtifacts,
+			.. BossArtifacts,
+		];
+
+	private static readonly IEnumerable<Type> RegisterableTypes
+		= [
+			.. AllCardTypes,
+			.. AllArtifactTypes,
+			.. DuoArtifacts,
+			typeof(PackManager),
+		];
+
+	public ModEntry(IPluginPackage<IModManifest> package, IModHelper helper, ILogger logger) : base(package, helper, logger)
+	{
+		Instance = this;
+		Harmony = helper.Utilities.Harmony;
+		// KokoroApi = helper.ModRegistry.GetApi<IKokoroApi>("Shockah.Kokoro")!.V2;
+
+		this.AnyLocalizations = new JsonLocalizationProvider(
+			tokenExtractor: new SimpleLocalizationTokenExtractor(),
+			localeStreamFunction: locale => package.PackageRoot.GetRelativeFile($"i18n/{locale}.json").OpenRead()
+		);
+		this.Localizations = new MissingPlaceholderLocalizationProvider<IReadOnlyList<string>>(
+			new CurrentLocaleOrEnglishLocalizationProvider<IReadOnlyList<string>>(this.AnyLocalizations)
+		);
+
+		BjornDeck = helper.Content.Decks.RegisterDeck("Gary", new()
+		{
+			Definition = new() { color = new("23EEB6"), titleColor = Colors.black },
+			DefaultCardArt = StableSpr.cards_colorless,
+			BorderSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/CardFrame.png")).Sprite,
+			Name = this.AnyLocalizations.Bind(["character", "name"]).Localize,
+			ShineColorOverride = _ => DB.decks[BjornDeck!.Deck].color.normalize().gain(2.5),
+		});
+
+		foreach (var type in RegisterableTypes)
+			AccessTools.DeclaredMethod(type, nameof(IRegisterable.Register))?.Invoke(null, [package, helper]);
+
+		helper.Content.Characters.V2.RegisterPlayableCharacter("Gary", new()
+		{
+			Deck = BjornDeck.Deck,
+			Description = this.AnyLocalizations.Bind(["character", "description"]).Localize,
+			BorderSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/CharacterFrame.png")).Sprite,
+			NeutralAnimation = new()
+			{
+				CharacterType = BjornDeck.UniqueName,
+				LoopTag = "neutral",
+				Frames = package.PackageRoot.GetRelativeDirectory("assets/Character/Neutral")
+					.GetSequentialFiles(i => $"{i}.png")
+					.Select(f => helper.Content.Sprites.RegisterSprite(f).Sprite)
+					.ToList()
+			},
+			MiniAnimation = new()
+			{
+				CharacterType = BjornDeck.UniqueName,
+				LoopTag = "mini",
+				Frames = [
+					helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Character/Mini.png")).Sprite
+				]
+			},
+			Starters = new()
+			{
+				cards = [
+				]
+			},
+			//ExeCardType = typeof(BlochExeCard),
+		});
+		
+		helper.Content.Characters.V2.RegisterCharacterAnimation(new()
+		{
+			CharacterType = BjornDeck.UniqueName,
+			LoopTag = "gameover",
+			Frames = package.PackageRoot.GetRelativeDirectory("assets/Character/GameOver")
+				.GetSequentialFiles(i => $"{i}.png")
+				.Select(f => helper.Content.Sprites.RegisterSprite(f).Sprite)
+				.ToList()
+		});
+		helper.Content.Characters.V2.RegisterCharacterAnimation(new()
+		{
+			CharacterType = BjornDeck.UniqueName,
+			LoopTag = "squint",
+			Frames = package.PackageRoot.GetRelativeDirectory("assets/Character/Squint")
+				.GetSequentialFiles(i => $"{i}.png")
+				.Select(f => helper.Content.Sprites.RegisterSprite(f).Sprite)
+				.ToList()
+		});
+
+		// helper.ModRegistry.AwaitApi<IMoreDifficultiesApi>(
+		// 	"TheJazMaster.MoreDifficulties",
+		// 	new SemanticVersion(1, 3, 0),
+		// 	api => api.RegisterAltStarters(
+		// 		deck: BjornDeck.Deck,
+		// 		starterDeck: new StarterDeck
+		// 		{
+		// 			cards = [
+		// 				new SmartShieldDroneCard(),
+		// 				new TaserCard(),
+		// 			]
+		// 		}
+		// 	)
+		// );
+	}
+
+	// public override object GetApi(IModManifest requestingMod)
+	// 	=> new ApiImplementation();
+
+	internal static Rarity GetCardRarity(Type type)
+	{
+		if (RareCardTypes.Contains(type))
+			return Rarity.rare;
+		if (UncommonCardTypes.Contains(type))
+			return Rarity.uncommon;
+		return Rarity.common;
+	}
+
+	internal static ArtifactPool[] GetArtifactPools(Type type)
+	{
+		if (BossArtifacts.Contains(type))
+			return [ArtifactPool.Boss];
+		if (CommonArtifacts.Contains(type))
+			return [ArtifactPool.Common];
+		return [];
+	}
+}
