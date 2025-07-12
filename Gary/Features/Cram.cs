8c6473bfe08c577d8ae17a893517c6636bc372ec
Michał Dolaś <me@shockah.pl>
2025-07-12 20:48:05 +0200

    Gary: Jupiter Drone handling

--- Gary/Features/Cram.cs
+++ Gary/Features/Cram.cs
@@ -22,6 +22,7 @@ internal sealed class CramManager : IRegisterable
 	private static StuffBase? ObjectBeingLaunchedInto;
 	private static StuffBase? ObjectToPutLater;
 	private static bool ObjectIsBeingCrammedInto;
+	private static Guid? NestedJupiterShootBeginId;
 	
 	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
 	{
@@ -79,6 +80,23 @@ internal sealed class CramManager : IRegisterable
 			original: AccessTools.DeclaredMethod(typeof(Combat), nameof(Combat.BeginCardAction)),
 			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Combat_BeginCardAction_Prefix)),
 			finalizer: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(Combat_BeginCardAction_Finalizer))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AAttack), nameof(AAttack.GetTooltips)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AAttack_GetTooltips_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AAttack), nameof(AAttack.DoWeHaveCannonsThough)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AAttack_DoWeHaveCannonsThough_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AJupiterShoot), nameof(AJupiterShoot.Begin)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AJupiterShoot_Begin_Prefix)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AJupiterShoot_Begin_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(JupiterDroneHubV2), nameof(JupiterDroneHubV2.OnPlayerSpawnSomething)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(JupiterDroneHubV2_OnPlayerSpawnSomething_Prefix))
 		);
 	}
 
@@ -424,5 +442,138 @@ internal sealed class CramManager : IRegisterable
 		__instance.stuff[e.WorldX] = e.RealObject;
 		if (existingObject != e.CrammedObject)
 			RemoveCrammedObject(__instance, e.WorldX, e.CrammedObject);
+	}
+
+	private static void AAttack_GetTooltips_Postfix(State s)
+	{
+		if (s.route is not Combat combat)
+			return;
+
+		foreach (var @object in combat.stuff.Values)
+		{
+			if (GetCrammedObjects(@object) is not { } crammedObjects)
+				continue;
+			foreach (var crammedObject in crammedObjects)
+				if (crammedObject is JupiterDrone)
+					crammedObject.hilight = 2;
+		}
+	}
+
+	private static void AAttack_DoWeHaveCannonsThough_Postfix(State s, ref bool __result)
+	{
+		if (__result)
+			return;
+		if (s.route is not Combat combat)
+			return;
+		
+		foreach (var @object in combat.stuff.Values)
+		{
+			if (GetCrammedObjects(@object) is not { } crammedObjects)
+				continue;
+			foreach (var crammedObject in crammedObjects)
+			{
+				if (crammedObject is not JupiterDrone)
+					continue;
+				__result = true;
+				return;
+			}
+		}
+	}
+
+	private static void AJupiterShoot_Begin_Prefix(AJupiterShoot __instance, out Guid __state)
+	{
+		__state = NestedJupiterShootBeginId ?? Guid.NewGuid();
+		ModEntry.Instance.Helper.ModData.SetModData(__instance.attackCopy, "IsFromAJupiterShoot", __state);
+	}
+
+	private static void AJupiterShoot_Begin_Postfix(AJupiterShoot __instance, G g, State s, Combat c, in Guid __state)
+	{
+		if (NestedJupiterShootBeginId is not null)
+			return;
+		
+		List<(int WorldX, AAttack Attack, StuffBase TopObject, StuffBase? CrammedObject, int Depth)> attacks = [];
+		for (var i = c.cardActions.Count - 1; i >= 0; i--)
+		{
+			if (c.cardActions[i] is not AAttack attack)
+				continue;
+			if (!ModEntry.Instance.Helper.ModData.TryGetModData<Guid>(attack, "IsFromAJupiterShoot", out var instanceId))
+				continue;
+			if (instanceId != __state)
+				continue;
+			if (attack.fromDroneX is null || !c.stuff.TryGetValue(attack.fromDroneX.Value, out var @object))
+				continue;
+
+			attacks.Add((attack.fromDroneX.Value, attack, @object, null, 0));
+			c.cardActions.RemoveAt(i);
+		}
+
+		NestedJupiterShootBeginId = __state;
+		var realStuff = c.stuff;
+		
+		try
+		{
+			List<(int WorldX, StuffBase TopObject, JupiterDrone JupiterDrone, int Depth)> crammedJupitedDrones = [];
+
+			foreach (var kvp in c.stuff)
+				if (GetCrammedObjects(kvp.Value) is { } crammedObjects)
+					for (var i = 0; i < crammedObjects.Count; i++)
+						if (crammedObjects[i] is JupiterDrone jupiterDrone)
+							crammedJupitedDrones.Add((kvp.Key, kvp.Value, jupiterDrone, i + 1));
+				
+			c.stuff = [];
+
+			while (crammedJupitedDrones.Count != 0)
+			{
+				var entry = crammedJupitedDrones[^1];
+				crammedJupitedDrones.RemoveAt(crammedJupitedDrones.Count - 1);
+				c.stuff[entry.WorldX] = entry.JupiterDrone;
+				
+				__instance.Begin(g, s, c);
+				
+				for (var i = c.cardActions.Count - 1; i >= 0; i--)
+				{
+					if (c.cardActions[i] is not AAttack attack)
+						continue;
+					if (!ModEntry.Instance.Helper.ModData.TryGetModData<Guid>(attack, "IsFromAJupiterShoot", out var instanceId))
+						continue;
+					if (instanceId != __state)
+						continue;
+					if (attack.fromDroneX is null || !c.stuff.ContainsKey(attack.fromDroneX.Value))
+						continue;
+
+					var crammedObjectId = ModEntry.Instance.Helper.ModData.ObtainModData(entry.JupiterDrone, "CrammedObjectId", Guid.NewGuid);
+					ModEntry.Instance.Helper.ModData.SetModData(attack, "ForceCrammedObjectId", crammedObjectId);
+					ModEntry.Instance.Helper.ModData.SetModData(attack, "ForceCrammedObjectWorldX", entry.WorldX);
+					
+					attacks.Add((entry.WorldX, attack, entry.TopObject, entry.JupiterDrone, entry.Depth));
+					c.cardActions.RemoveAt(i);
+				}
+			}
+		}
+		finally
+		{
+			c.stuff = realStuff;
+			NestedJupiterShootBeginId = null;
+		}
+		
+		c.QueueImmediate(
+			attacks
+				.OrderBy(e => e.WorldX)
+				.ThenBy(e => e.Depth)
+				.Select(e => e.Attack)
+		);
+	}
+
+	private static bool JupiterDroneHubV2_OnPlayerSpawnSomething_Prefix(Combat combat, StuffBase thing)
+	{
+		if (thing is not JupiterDrone)
+			return true;
+		
+		foreach (var @object in combat.stuff.Values)
+			if (GetCrammedObjects(@object) is { } crammedObjects)
+				if (crammedObjects.Any(crammedObject => crammedObject is JupiterDrone))
+					return false;
+
+		return true;
 	}
 }
