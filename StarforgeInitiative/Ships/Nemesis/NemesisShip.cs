8526eb251db6ad47cb37b2e4da70d148d6e8a283
Michał Dolaś <me@shockah.pl>
2025-09-14 15:30:27 +0200

    StarforgeInitiative: WIP Nemesis art

--- StarforgeInitiative/Ships/Nemesis/NemesisShip.cs
+++ StarforgeInitiative/Ships/Nemesis/NemesisShip.cs
@@ -26,9 +26,33 @@ internal sealed class NemesisShip : IRegisterable
 	];
 	
 	internal static IShipEntry ShipEntry { get; private set; } = null!;
+	internal static IPartEntry LeftCannonCommsEntry { get; private set; } = null!;
+	internal static IPartEntry LeftCannonEntry { get; private set; } = null!;
+	internal static IPartEntry MidCannonEntry { get; private set; } = null!;
+	internal static IPartEntry RightCannonEntry { get; private set; } = null!;
 	
 	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
 	{
+		LeftCannonCommsEntry = helper.Content.Ships.RegisterPart("NemesisCannonLeftComms", new()
+		{
+			Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonLeftComms.png")).Sprite,
+		});
+		LeftCannonEntry = helper.Content.Ships.RegisterPart("NemesisCannonLeft", new()
+		{
+			Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonLeftActive.png")).Sprite,
+			DisabledSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonLeftInactive.png")).Sprite,
+		});
+		MidCannonEntry = helper.Content.Ships.RegisterPart("NemesisCannonMid", new()
+		{
+			Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonMidActive.png")).Sprite,
+			DisabledSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonMidInactive.png")).Sprite,
+		});
+		RightCannonEntry = helper.Content.Ships.RegisterPart("NemesisCannonRight", new()
+		{
+			Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonRightActive.png")).Sprite,
+			DisabledSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/CannonRightInactive.png")).Sprite,
+		});
+		
 		ShipEntry = helper.Content.Ships.RegisterShip("Nemesis", new()
 		{
 			Name = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Nemesis", "name"]).Localize,
@@ -41,13 +65,39 @@ internal sealed class NemesisShip : IRegisterable
 					hullMax = 15,
 					shieldMaxBase = 5,
 					parts = [
-						new() { type = PType.wing, skin = "wing_bruiser", damageModifier = PDamMod.armor },
-						new() { type = PType.comms, skin = "wing_jupiter_b" },
-						new() { type = PType.missiles, skin = "missiles_sandwich" },
-						new() { type = PType.cannon, skin = "wing_ares", active = false },
-						new() { type = PType.cockpit, skin = "cockpit_goliath" },
-						new() { type = PType.cannon, skin = "wing_ares", flip = true },
-						new() { type = PType.wing, skin = "wing_bruiser", flip = true, damageModifier = PDamMod.armor },
+						new() {
+							type = PType.wing,
+							skin = helper.Content.Ships.RegisterPart("NemesisWingLeft", new()
+							{
+								Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/WingLeft.png")).Sprite
+							}).UniqueName,
+							damageModifier = PDamMod.armor,
+						},
+						new() { type = PType.comms, skin = LeftCannonCommsEntry.UniqueName },
+						new() {
+							type = PType.missiles,
+							skin = helper.Content.Ships.RegisterPart("NemesisBay", new()
+							{
+								Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/Bay.png")).Sprite
+							}).UniqueName,
+						},
+						new() { type = PType.cannon, skin = MidCannonEntry.UniqueName, active = false },
+						new() {
+							type = PType.cockpit,
+							skin = helper.Content.Ships.RegisterPart("NemesisCockpit", new()
+							{
+								Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/Cockpit.png")).Sprite
+							}).UniqueName,
+						},
+						new() { type = PType.cannon, skin = RightCannonEntry.UniqueName },
+						new() {
+							type = PType.wing,
+							skin = helper.Content.Ships.RegisterPart("NemesisWingRight", new()
+							{
+								Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/WingRight.png")).Sprite
+							}).UniqueName,
+							damageModifier = PDamMod.armor,
+						},
 					]
 				},
 				artifacts = [
@@ -62,7 +112,7 @@ internal sealed class NemesisShip : IRegisterable
 					new NemesisBasicDualDroneCard(),
 				],
 			},
-			UnderChassisSprite = StableSpr.parts_chassis_bramblepelt,
+			UnderChassisSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Nemesis/Ship/Chassis.png")).Sprite,
 			ExclusiveArtifactTypes = ArtifactTypes.ToHashSet(),
 		});
 		
