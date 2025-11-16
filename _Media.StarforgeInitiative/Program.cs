449532f2afc08106acdf310ddae8916dad6af9fd
Michał Dolaś <me@shockah.pl>
2025-11-16 22:44:56 +0100

    StarforgeInitiative: Banner update

--- _Media.StarforgeInitiative/Program.cs
+++ _Media.StarforgeInitiative/Program.cs
@@ -15,6 +15,7 @@ var font = SKTypeface.FromFile(Path.Combine(GetRootPath(), "..", "_MediaCommon",
 var background = SKImage.FromEncodedData(Path.Combine(GetRootPath(), "assets", "Background.jpg"));
 var breadnaught = SKImage.FromEncodedData(Path.Combine(GetRootPath(), "assets", "Breadnaught.png"));
 var kepler = SKImage.FromEncodedData(Path.Combine(GetRootPath(), "assets", "Kepler.png"));
+var nemesis = SKImage.FromEncodedData(Path.Combine(GetRootPath(), "assets", "Nemesis.png"));
 
 canvas.Clear(SKColors.Empty);
 canvas.DrawImage(background, new SKRect(0, 0, imageInfo.Width, imageInfo.Height));
@@ -27,8 +28,8 @@ using (var paint = new SKPaint())
 		var sprite = breadnaught;
 		var width = sprite.Width * shipScale;
 		var height = sprite.Height * shipScale;
-		var originX = imageInfo.Width / 2 - (int)(imageInfo.Width * 0.2);
-		var originY = imageInfo.Height / 2 + (int)(imageInfo.Height * 0.1);
+		var originX = imageInfo.Width / 2 - (int)(imageInfo.Width * 0.25);
+		var originY = imageInfo.Height / 2 + (int)(imageInfo.Height * 0.2);
 		canvas.DrawImage(sprite, new SKRect(originX - width / 2, originY - height / 2, originX + width / 2, originY + height / 2), paint);
 	}
 	
@@ -36,8 +37,17 @@ using (var paint = new SKPaint())
 		var sprite = kepler;
 		var width = sprite.Width * shipScale;
 		var height = sprite.Height * shipScale;
+		var originX = imageInfo.Width / 2 - (int)(imageInfo.Width * 0.25);
+		var originY = imageInfo.Height / 2 - (int)(imageInfo.Height * 0.2);
+		canvas.DrawImage(sprite, new SKRect(originX - width / 2, originY - height / 2, originX + width / 2, originY + height / 2), paint);
+	}
+	
+	{
+		var sprite = nemesis;
+		var width = sprite.Width * shipScale;
+		var height = sprite.Height * shipScale;
 		var originX = imageInfo.Width / 2 + (int)(imageInfo.Width * 0.2);
-		var originY = imageInfo.Height / 2 - (int)(imageInfo.Height * 0.1);
+		var originY = imageInfo.Height / 2 - (int)(imageInfo.Height * 0.0);
 		canvas.DrawImage(sprite, new SKRect(originX - width / 2, originY - height / 2, originX + width / 2, originY + height / 2), paint);
 	}
 }
