b349a6a80e11cb7519bd17220870c6469697bdec
Michał Dolaś <me@shockah.pl>
2025-11-21 23:06:29 +0100

    Gary: Speed Buff card

---
+++ Gary/Cards/SpeedBuffCard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Kokoro;
+using Shockah.Shared;
+
+namespace Shockah.Gary;
+
+public sealed class SpeedBuffCard : Card, IRegisterable, IHasCustomCardTraits
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		var entry = helper.Content.Cards.RegisterCard(MethodBase.GetCurrentMethod()!.DeclaringType!.Name, new()
+		{
+			CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				deck = ModEntry.Instance.GaryDeck.Deck,
+				rarity = ModEntry.GetCardRarity(MethodBase.GetCurrentMethod()!.DeclaringType!),
+				upgradesTo = [Upgrade.A, Upgrade.B],
+			},
+			Art = helper.Content.Sprites.RegisterSpriteOrDefault(package.PackageRoot.GetRelativeFile("assets/Cards/SpeedBuff.png"), StableSpr.cards_Dodge).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "SpeedBuff", "name"]).Localize,
+		});
+		
+		ModEntry.Instance.KokoroApi.Finite.SetBaseFiniteUses(entry.UniqueName, Upgrade.None, 2);
+		ModEntry.Instance.KokoroApi.Finite.SetBaseFiniteUses(entry.UniqueName, Upgrade.A, 2);
+		ModEntry.Instance.KokoroApi.Finite.SetBaseFiniteUses(entry.UniqueName, Upgrade.B, 4);
+	}
+
+	public override CardData GetData(State state)
+		=> upgrade switch
+		{
+			Upgrade.A => new() { cost = 1, retain = true },
+			_ => new() { cost = 1 },
+		};
+
+	public IReadOnlySet<ICardTraitEntry> GetInnateTraits(State state)
+		=> new HashSet<ICardTraitEntry> { ModEntry.Instance.KokoroApi.Finite.Trait };
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> [
+			new AStatus { targetPlayer = true, status = Status.evade, statusAmount = 1 },
+			ModEntry.Instance.KokoroApi.Sequence.MakeAction(
+				uuid, IKokoroApi.IV2.ISequenceApi.Interval.Turn, 2, 2,
+				new AStatus { targetPlayer = true, status = Status.hermes, statusAmount = 1 }
+			).AsCardAction,
+		];
+}
