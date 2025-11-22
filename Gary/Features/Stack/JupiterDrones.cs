a48390f9c4260de7a96a4b0312d445c703169277
Michał Dolaś <me@shockah.pl>
2025-11-22 11:33:41 +0100

    Gary: Split the Stack file

---
+++ Gary/Features/Stack/JupiterDrones.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+
+namespace Shockah.Gary;
+
+internal partial class Stack
+{
+	private static Guid? NestedJupiterShootBeginId;
+	
+	private static void HandleJupiterDrones()
+	{
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AAttack), nameof(AAttack.GetTooltips)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AAttack_GetTooltips_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AAttack), nameof(AAttack.DoWeHaveCannonsThough)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AAttack_DoWeHaveCannonsThough_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AJupiterShoot), nameof(AJupiterShoot.Begin)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AJupiterShoot_Begin_Prefix)),
+			postfix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AJupiterShoot_Begin_Postfix))
+		);
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(JupiterDroneHubV2), nameof(JupiterDroneHubV2.OnPlayerSpawnSomething)),
+			prefix: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(JupiterDroneHubV2_OnPlayerSpawnSomething_Prefix))
+		);
+	}
+	
+	private static void AAttack_GetTooltips_Postfix(State s)
+	{
+		if (s.route is not Combat combat)
+			return;
+		
+		ApplyToAllStackedObjects(combat, @object =>
+		{
+			if (@object is JupiterDrone)
+				@object.hilight = 2;
+		});
+	}
+
+	private static void AAttack_DoWeHaveCannonsThough_Postfix(State s, ref bool __result)
+	{
+		if (__result)
+			return;
+		if (s.route is not Combat combat)
+			return;
+		
+		if (AnyStackedObject(combat, @object => @object is JupiterDrone))
+			__result = true;
+	}
+
+	private static void AJupiterShoot_Begin_Prefix(AJupiterShoot __instance, out Guid __state)
+	{
+		__state = NestedJupiterShootBeginId ?? Guid.NewGuid();
+		ModEntry.Instance.Helper.ModData.SetModData(__instance.attackCopy, "IsFromAJupiterShoot", __state);
+	}
+
+	private static void AJupiterShoot_Begin_Postfix(AJupiterShoot __instance, G g, State s, Combat c, in Guid __state)
+	{
+		if (NestedJupiterShootBeginId is not null)
+			return;
+		
+		List<(int WorldX, AAttack Attack, StuffBase TopObject, StuffBase? StackedObject, int Depth)> attacks = [];
+		for (var i = c.cardActions.Count - 1; i >= 0; i--)
+		{
+			if (c.cardActions[i] is not AAttack attack)
+				continue;
+			if (!ModEntry.Instance.Helper.ModData.TryGetModData<Guid>(attack, "IsFromAJupiterShoot", out var instanceId))
+				continue;
+			if (instanceId != __state)
+				continue;
+			if (attack.fromDroneX is null || !c.stuff.TryGetValue(attack.fromDroneX.Value, out var @object))
+				continue;
+
+			attacks.Add((attack.fromDroneX.Value, attack, @object, null, 0));
+			c.cardActions.RemoveAt(i);
+		}
+
+		NestedJupiterShootBeginId = __state;
+		var realStuff = c.stuff;
+		
+		try
+		{
+			List<(int WorldX, StuffBase TopObject, JupiterDrone JupiterDrone, int Depth)> stackedJupitedDrones = [];
+
+			foreach (var kvp in c.stuff)
+				if (GetStackedObjects(kvp.Value) is { } stackedObjects)
+					for (var i = 0; i < stackedObjects.Count; i++)
+						if (stackedObjects[i] is JupiterDrone jupiterDrone)
+							stackedJupitedDrones.Add((kvp.Key, kvp.Value, jupiterDrone, i + 1));
+				
+			c.stuff = [];
+
+			while (stackedJupitedDrones.Count != 0)
+			{
+				var entry = stackedJupitedDrones[^1];
+				stackedJupitedDrones.RemoveAt(stackedJupitedDrones.Count - 1);
+				c.stuff[entry.WorldX] = entry.JupiterDrone;
+				
+				__instance.Begin(g, s, c);
+				
+				for (var i = c.cardActions.Count - 1; i >= 0; i--)
+				{
+					if (c.cardActions[i] is not AAttack attack)
+						continue;
+					if (!ModEntry.Instance.Helper.ModData.TryGetModData<Guid>(attack, "IsFromAJupiterShoot", out var instanceId))
+						continue;
+					if (instanceId != __state)
+						continue;
+					if (attack.fromDroneX is null || !c.stuff.ContainsKey(attack.fromDroneX.Value))
+						continue;
+
+					var stackedObjectId = ObtainStackedObjectId(entry.JupiterDrone);
+					ModEntry.Instance.Helper.ModData.SetModData(attack, "ForceStackedObjectId", stackedObjectId);
+					ModEntry.Instance.Helper.ModData.SetModData(attack, "ForceStackedObjectWorldX", entry.WorldX);
+					
+					attacks.Add((entry.WorldX, attack, entry.TopObject, entry.JupiterDrone, entry.Depth));
+					c.cardActions.RemoveAt(i);
+				}
+			}
+		}
+		finally
+		{
+			c.stuff = realStuff;
+			NestedJupiterShootBeginId = null;
+		}
+		
+		c.QueueImmediate(
+			attacks
+				.OrderBy(e => e.WorldX)
+				.ThenBy(e => e.Depth)
+				.Select(e => e.Attack)
+		);
+	}
+
+	private static bool JupiterDroneHubV2_OnPlayerSpawnSomething_Prefix(Combat combat, StuffBase thing)
+	{
+		if (thing is not JupiterDrone)
+			return true;
+
+		if (AnyStackedObject(combat, @object => @object is JupiterDrone))
+			return false;
+
+		return true;
+	}
+}
---
+++ Gary/Features/Stack/Missiles.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Microsoft.Extensions.Logging;
+using Nanoray.Shrike;
+using Nanoray.Shrike.Harmony;
+
+namespace Shockah.Gary;
+
+internal partial class Stack
+{
+	private static void HandleMissiles()
+	{
+		ModEntry.Instance.Harmony.Patch(
+			original: AccessTools.DeclaredMethod(typeof(AMissileHit), nameof(AMissileHit.Update)),
+			transpiler: new HarmonyMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AMissileHit_Update_Transpiler))
+		);
+	}
+	
+	[SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+	private static IEnumerable<CodeInstruction> AMissileHit_Update_Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase originalMethod, ILGenerator il)
+	{
+		try
+		{
+			return new SequenceBlockMatcher<CodeInstruction>(instructions)
+				.PointerMatcher(SequenceMatcherRelativeElement.First)
+				.CreateLabel(il, out var label)
+				.Insert(SequenceMatcherPastBoundsDirection.Before, SequenceMatcherInsertionResultingBounds.IncludingInsertion, [
+					new CodeInstruction(OpCodes.Ldarg_0),
+					new CodeInstruction(OpCodes.Ldarg_1),
+					new CodeInstruction(OpCodes.Ldarg_3),
+					new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AMissileHit_Update_Transpiler_ShouldContinue))),
+					new CodeInstruction(OpCodes.Brtrue, label),
+					new CodeInstruction(OpCodes.Ret),
+				])
+				.Find(ILMatches.Stloc<Missile>(originalMethod).GetLocalIndex(out var missileLocalIndex))
+				.Find([
+					ILMatches.Ldarg(3),
+					ILMatches.Ldfld(nameof(Combat.stuff)),
+					ILMatches.Ldarg(0),
+					ILMatches.Ldfld(nameof(AMissileHit.worldX)),
+					ILMatches.Call("Remove"),
+					ILMatches.Instruction(OpCodes.Pop),
+				])
+				.Insert(SequenceMatcherPastBoundsDirection.After, SequenceMatcherInsertionResultingBounds.IncludingInsertion, [
+					new CodeInstruction(OpCodes.Ldarg_0),
+					new CodeInstruction(OpCodes.Ldarg_3),
+					new CodeInstruction(OpCodes.Ldloc, missileLocalIndex.Value),
+					new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(MethodBase.GetCurrentMethod()!.DeclaringType!, nameof(AMissileHit_Update_Transpiler_PutStackBack))),
+				])
+				.AllElements();
+		}
+		catch (Exception ex)
+		{
+			ModEntry.Instance.Logger.LogError("Could not patch method {Method} - {Mod} probably won't work.\nReason: {Exception}", originalMethod, ModEntry.Instance.Package.Manifest.UniqueName, ex);
+			return instructions;
+		}
+	}
+
+	private static bool AMissileHit_Update_Transpiler_ShouldContinue(AMissileHit action, G g, Combat combat)
+	{
+		if (!combat.stuff.TryGetValue(action.worldX, out var @object))
+			return true;
+
+		if (!ModEntry.Instance.Helper.ModData.TryGetModData<Guid>(action, "ChecksForStackedObjectId", out var checksForStackedObjectId))
+		{
+			checksForStackedObjectId = ObtainStackedObjectId(@object);
+			ModEntry.Instance.Helper.ModData.SetModData(action, "ChecksForStackedObjectId", checksForStackedObjectId);
+		}
+
+		if (checksForStackedObjectId == ObtainStackedObjectId(@object))
+			return true;
+
+		action.timer -= g.dt;
+		return false;
+	}
+
+	private static void AMissileHit_Update_Transpiler_PutStackBack(AMissileHit action, Combat combat, Missile missile)
+	{
+		combat.stuff[action.worldX] = missile;
+		RemoveStackedObject(combat, action.worldX, missile);
+	}
+}
