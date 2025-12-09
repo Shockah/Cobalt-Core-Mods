0ce1104be49012dfe847ed66082039eb1b652cb0
Michał Dolaś <me@shockah.pl>
2025-12-09 15:05:40 +0100

    StarforgeInitiative: Vulcan's Water Cooling is now limited to once per turn

--- StarforgeInitiative/Ships/Vulcan/Artifacts/VulcanWaterCoolingArtifact.cs
+++ StarforgeInitiative/Ships/Vulcan/Artifacts/VulcanWaterCoolingArtifact.cs
@@ -1,13 +1,23 @@
 ﻿using System.Reflection;
 using Nanoray.PluginManager;
+using Newtonsoft.Json;
 using Nickel;
 
 namespace Shockah.StarforgeInitiative;
 
 internal sealed class VulcanWaterCoolingArtifact : Artifact, IRegisterable
 {
+	private static ISpriteEntry ActiveSprite = null!;
+	private static ISpriteEntry InactiveSprite = null!;
+	
+	[JsonProperty]
+	internal bool TriggeredThisTurn;
+	
 	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
 	{
+		ActiveSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Vulcan/Artifact/WaterCooling.png"));
+		InactiveSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/Vulcan/Artifact/WaterCoolingInactive.png"));
+		
 		helper.Content.Artifacts.RegisterArtifact("VulcanWaterCooling", new()
 		{
 			ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
@@ -17,9 +27,18 @@ internal sealed class VulcanWaterCoolingArtifact : Artifact, IRegisterable
 				pools = [ArtifactPool.Common],
 				unremovable = true,
 			},
-			Sprite = helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Vulcan/Artifact/WaterCooling.png")).Sprite,
+			Sprite = ActiveSprite.Sprite,
 			Name = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Vulcan", "artifact", "WaterCooling", "name"]).Localize,
 			Description = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Vulcan", "artifact", "WaterCooling", "description"]).Localize
 		});
+	}
+
+	public override Spr GetSprite()
+		=> TriggeredThisTurn ? InactiveSprite.Sprite : ActiveSprite.Sprite;
+
+	public override void OnTurnStart(State state, Combat combat)
+	{
+		base.OnTurnStart(state, combat);
+		TriggeredThisTurn = false;
 	}
 }
