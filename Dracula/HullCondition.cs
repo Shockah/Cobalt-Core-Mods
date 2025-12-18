832991dad3b7df530f8c48c0f11f82a3a01dd0a2
Michał Dolaś <me@shockah.pl>
2025-12-18 16:37:39 +0100

    Dracula: Mavis WIP

---
+++ Dracula/HullCondition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Nickel;
+using Shockah.Kokoro;
+
+namespace Shockah.Dracula;
+
+public sealed class HullCondition : IKokoroApi.IV2.IConditionalApi.IBoolExpression
+{
+	public required bool BelowHalf;
+
+	public bool GetValue(State state, Combat combat)
+		=> BelowHalf ? state.ship.hull <= state.ship.hullMax / 2 : state.ship.hull > state.ship.hullMax / 2;
+
+	public string GetTooltipDescription(State state, Combat? combat)
+	{
+		if (state.IsOutsideRun() || state == DB.fakeState)
+			return ModEntry.Instance.Localizations.Localize(["condition", "hull", BelowHalf ? "below" : "above", "stateless"]);
+		else
+			return ModEntry.Instance.Localizations.Localize(["condition", "hull", BelowHalf ? "below" : "above", "stateful"], new { Hull = state.ship.hullMax / 2 });
+	}
+
+	public void Render(G g, ref Vec position, bool isDisabled, bool dontRender)
+	{
+		if (!dontRender)
+			Draw.Sprite(
+				(BelowHalf ? ModEntry.Instance.HullBelowHalf : ModEntry.Instance.HullAboveHalf).Sprite,
+				position.x,
+				position.y,
+				color: isDisabled ? Colors.disabledIconTint : Colors.white
+			);
+		position.x += 8;
+	}
+
+	public IEnumerable<Tooltip> OverrideConditionalTooltip(State state, Combat? combat, Tooltip defaultTooltip, string defaultTooltipDescription)
+		=> [
+			new GlossaryTooltip($"AConditional::{ModEntry.Instance.Package.Manifest.UniqueName}::HullCondition::BelowHalf={BelowHalf}")
+			{
+				Icon = (BelowHalf ? ModEntry.Instance.HullBelowHalf : ModEntry.Instance.HullAboveHalf).Sprite,
+				TitleColor = Colors.action,
+				Title = ModEntry.Instance.Localizations.Localize(["condition", "hull", BelowHalf ? "below" : "above", "title"]),
+				Description = defaultTooltipDescription,
+			}
+		];
+}
