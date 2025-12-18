71e59e0d5e3fed9363072b4ae159233fdb031a10
Michał Dolaś <me@shockah.pl>
2025-12-18 16:37:39 +0100

    Dracula: Mavis WIP

---
+++ Dracula/Cards/Mavis/BraceCard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.Dracula;
+
+internal sealed class BraceCard : Card, IDraculaCard
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Cards.RegisterCard("Brace", new()
+		{
+			CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				deck = ModEntry.Instance.MavisDeck.Deck,
+				rarity = Rarity.common,
+				upgradesTo = [Upgrade.A, Upgrade.B]
+			},
+			Art = StableSpr.cards_HandCannon,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Mavis", "Brace", "name"]).Localize
+		});
+	}
+
+	public override CardData GetData(State state)
+		=> new() { cost = 1 };
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> upgrade switch
+		{
+			Upgrade.A => [
+				ModEntry.Instance.KokoroApi.TempHull.MakeGainAction(2).AsCardAction,
+			],
+			Upgrade.B => [
+				ModEntry.Instance.KokoroApi.TempHull.MakeGainAction(2).AsCardAction,
+				ModEntry.Instance.KokoroApi.Conditional.MakeAction(
+					new HullCondition { BelowHalf = true },
+					ModEntry.Instance.KokoroApi.TempHull.MakeGainAction(1).AsCardAction
+				).AsCardAction,
+			],
+			_ => [
+				ModEntry.Instance.KokoroApi.TempHull.MakeGainAction(1).AsCardAction,
+				ModEntry.Instance.KokoroApi.Conditional.MakeAction(
+					new HullCondition { BelowHalf = true },
+					ModEntry.Instance.KokoroApi.TempHull.MakeGainAction(1).AsCardAction
+				).AsCardAction,
+			]
+		};
+}
---
+++ Dracula/Cards/Mavis/TearCard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.Dracula;
+
+internal sealed class TearCard : Card, IDraculaCard
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Cards.RegisterCard("Tear", new()
+		{
+			CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				deck = ModEntry.Instance.MavisDeck.Deck,
+				rarity = Rarity.common,
+				upgradesTo = [Upgrade.A, Upgrade.B]
+			},
+			Art = StableSpr.cards_HandCannon,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Mavis", "Tear", "name"]).Localize
+		});
+	}
+
+	public override CardData GetData(State state)
+		=> new() { cost = 1 };
+
+	public override List<CardAction> GetActions(State s, Combat c)
+		=> upgrade switch
+		{
+			Upgrade.A => [
+				new AHurt { targetPlayer = false, hurtShieldsFirst = true, hurtAmount = 1 },
+				new AAttack { damage = GetDmg(s, 0), status = ModEntry.Instance.BleedingStatus.Status, statusAmount = 2 },
+			],
+			Upgrade.B => [
+				ModEntry.Instance.KokoroApi.ActionCosts.MakeCostAction(
+					ModEntry.Instance.KokoroApi.ActionCosts.MakeResourceCost(ModEntry.Instance.KokoroApi.ActionCosts.MakeStatusResource(ModEntry.Instance.BleedingStatus.Status, false), 1),
+					new AHurt { targetPlayer = false, hurtAmount = 1 }
+				).AsCardAction,
+				new AAttack { damage = GetDmg(s, 0), status = ModEntry.Instance.BleedingStatus.Status, statusAmount = 2 },
+			],
+			_ => [
+				ModEntry.Instance.KokoroApi.ActionCosts.MakeCostAction(
+					ModEntry.Instance.KokoroApi.ActionCosts.MakeResourceCost(ModEntry.Instance.KokoroApi.ActionCosts.MakeStatusResource(ModEntry.Instance.BleedingStatus.Status, false), 1),
+					new AHurt { targetPlayer = false, hurtShieldsFirst = true, hurtAmount = 1 }
+				).AsCardAction,
+				new AAttack { damage = GetDmg(s, 0), status = ModEntry.Instance.BleedingStatus.Status, statusAmount = 2 },
+			]
+		};
+}
