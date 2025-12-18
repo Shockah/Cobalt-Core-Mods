9337767c69f3991390d27d4b28a7385f83346f39
Michał Dolaś <me@shockah.pl>
2025-12-18 16:37:39 +0100

    Dracula: Mavis WIP

--- Dracula/Cards/Dracula/DeathCoilCard.cs
+++ Dracula/Cards/Dracula/DeathCoilCard.cs
@@ -1,9 +1,8 @@
-﻿using Nanoray.PluginManager;
-using Nickel;
-using Shockah.Kokoro;
-using Shockah.Shared;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Shared;
 
 namespace Shockah.Dracula;
 
@@ -89,43 +88,4 @@ internal sealed class DeathCoilCard : Card, IDraculaCard
 				}
 			]
 		};
-
-	private sealed class HullCondition : IKokoroApi.IV2.IConditionalApi.IBoolExpression
-	{
-		public required bool BelowHalf;
-
-		public bool GetValue(State state, Combat combat)
-			=> BelowHalf ? state.ship.hull <= state.ship.hullMax / 2 : state.ship.hull > state.ship.hullMax / 2;
-
-		public string GetTooltipDescription(State state, Combat? combat)
-		{
-			if (state.IsOutsideRun() || state == DB.fakeState)
-				return ModEntry.Instance.Localizations.Localize(["condition", "hull", BelowHalf ? "below" : "above", "stateless"]);
-			else
-				return ModEntry.Instance.Localizations.Localize(["condition", "hull", BelowHalf ? "below" : "above", "stateful"], new { Hull = state.ship.hullMax / 2 });
-		}
-
-		public void Render(G g, ref Vec position, bool isDisabled, bool dontRender)
-		{
-			if (!dontRender)
-				Draw.Sprite(
-					(BelowHalf ? ModEntry.Instance.HullBelowHalf : ModEntry.Instance.HullAboveHalf).Sprite,
-					position.x,
-					position.y,
-					color: isDisabled ? Colors.disabledIconTint : Colors.white
-				);
-			position.x += 8;
-		}
-
-		public IEnumerable<Tooltip> OverrideConditionalTooltip(State state, Combat? combat, Tooltip defaultTooltip, string defaultTooltipDescription)
-			=> [
-				new GlossaryTooltip($"AConditional::{ModEntry.Instance.Package.Manifest.UniqueName}::HullCondition::BelowHalf={BelowHalf}")
-				{
-					Icon = (BelowHalf ? ModEntry.Instance.HullBelowHalf : ModEntry.Instance.HullAboveHalf).Sprite,
-					TitleColor = Colors.action,
-					Title = ModEntry.Instance.Localizations.Localize(["condition", "hull", BelowHalf ? "below" : "above", "title"]),
-					Description = defaultTooltipDescription,
-				}
-			];
-	}
 }
