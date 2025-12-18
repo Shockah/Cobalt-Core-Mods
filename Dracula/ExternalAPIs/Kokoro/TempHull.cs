77a0d0df952c43678c3f89d635c988802c1627b2
Michał Dolaś <me@shockah.pl>
2025-12-18 13:02:24 +0100

    Dracula: Empty Mavis character

---
+++ Dracula/ExternalAPIs/Kokoro/TempHull.cs
@@ -0,0 +1,83 @@
+﻿namespace Shockah.Kokoro;
+
+public partial interface IKokoroApi
+{
+	public partial interface IV2
+	{
+		/// <inheritdoc cref="ITempHullApi"/>
+		ITempHullApi TempHull { get; }
+
+		/// <summary>
+		/// Allows using actions and statuses which temporarily remove or grant hull.
+		/// </summary>
+		public interface ITempHullApi
+		{
+			/// <summary>
+			/// When you gain any, gain that much hull. When you lose any hull, lose that much TEMP HULL. Lose all of it and that much hull at turn start.
+			/// </summary>
+			Status TempHullStatus { get; }
+			
+			/// <summary>
+			/// Regain {0} hull at the end of combat, or at the end of a turn where you didn't suffer temp hull los. Does not count as healing.
+			/// </summary>
+			Status RegainHullLaterStatus { get; }
+			
+			/// <summary>
+			/// Casts the action as a temp hull loss action, if it is one.
+			/// </summary>
+			/// <param name="action">The potential temp hull loss action.</param>
+			/// <returns>The temp hull loss action, if the given action is one, or <c>null</c> otherwise.</returns>
+			ILoseAction? AsAction(CardAction action);
+			
+			/// <summary>
+			/// Creates a new on discard action, wrapping the provided action.
+			/// </summary>
+			/// <param name="amount">The amount of hull to lose.</param>
+			/// <param name="targetPlayer">Whether this action targets the player (<c>true</c>) or the enemy (<c>false</c>).</param>
+			/// <returns>The new on discard action.</returns>
+			ILoseAction MakeLoseAction(int amount, bool targetPlayer = true);
+			
+			/// <summary>
+			/// Represents an action, which only triggers on turn end, if the card is still in hand.
+			/// </summary>
+			public interface ILoseAction : ICardAction<CardAction>
+			{
+				/// <summary>
+				/// Whether this action targets the player (<c>true</c>) or the enemy (<c>false</c>).
+				/// </summary>
+				bool TargetPlayer { get; set; }
+				
+				/// <summary>
+				/// The amount of hull to lose.
+				/// </summary>
+				int Amount { get; set; }
+				
+				/// <summary>
+				/// Whether the action is prohibited from killing the target ship.
+				/// </summary>
+				bool CannotKill { get; set; }
+
+				/// <summary>
+				/// Sets <see cref="TargetPlayer"/>.
+				/// </summary>
+				/// <param name="value">The new value.</param>
+				/// <returns>This object after the change.</returns>
+				ILoseAction SetTargetPlayer(bool value);
+
+				/// <summary>
+				/// Sets <see cref="Amount"/>.
+				/// </summary>
+				/// <param name="value">The new value.</param>
+				/// <returns>This object after the change.</returns>
+				ILoseAction SetAmount(int value);
+
+				/// <summary>
+				/// Sets <see cref="CannotKill"/>.
+				/// </summary>
+				/// <param name="value">The new value.</param>
+				/// <returns>This object after the change.</returns>
+				ILoseAction SetCannotKill(bool value);
+			}
+		}
+	}
+}
