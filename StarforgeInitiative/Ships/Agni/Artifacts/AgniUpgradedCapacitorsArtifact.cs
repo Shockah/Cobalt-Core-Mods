c90b163a60d0fcbcc2334a050e7e385dc693d37a
Michał Dolaś <me@shockah.pl>
2025-12-29 23:35:02 +0100

    StarfogeInitiative: Renamed Vulcan to Agni

---
+++ StarforgeInitiative/Ships/Agni/Artifacts/AgniUpgradedCapacitorsArtifact.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.StarforgeInitiative;
+
+internal sealed class AgniUpgradedCapacitorsArtifact : Artifact, IRegisterable
+{
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		helper.Content.Artifacts.RegisterArtifact("AgniUpgradedCapacitors", new()
+		{
+			ArtifactType = MethodBase.GetCurrentMethod()!.DeclaringType!,
+			Meta = new()
+			{
+				owner = Deck.colorless,
+				pools = [ArtifactPool.Boss],
+				unremovable = true,
+			},
+			Sprite = helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/Agni/Artifact/UpgradedCapacitors.png")).Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Agni", "artifact", "UpgradedCapacitors", "name"]).Localize,
+			Description = ModEntry.Instance.AnyLocalizations.Bind(["ship", "Agni", "artifact", "UpgradedCapacitors", "description"]).Localize
+		});
+	}
+}
