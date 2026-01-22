cdeb1658a4fde951045d01600cb9bff772ba7451
Michał Dolaś <me@shockah.pl>
2026-01-22 20:14:08 +0100

    Johanna: WIP rewrite, cluster missiles WIP

---
+++ Johanna/GameEnums.cs
@@ -0,0 +1,6 @@
+﻿using Nanoray.EnumByNameSourceGenerator;
+
+namespace Shockah.Johanna;
+
+[EnumByName(typeof(Spr))]
+internal static partial class StableSpr;
---
+++ Johanna/ModEntry.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Microsoft.Extensions.Logging;
+using Nanoray.PluginManager;
+using Nickel;
+using Shockah.Kokoro;
+
+namespace Shockah.Johanna;
+
+public sealed class ModEntry : SimpleMod
+{
+	internal static ModEntry Instance { get; private set; } = null!;
+	internal readonly IHarmony Harmony;
+	internal IKokoroApi.IV2 KokoroApi { get; }
+	internal readonly ILocalizationProvider<IReadOnlyList<string>> AnyLocalizations;
+	internal readonly ILocaleBoundNonNullLocalizationProvider<IReadOnlyList<string>> Localizations;
+
+	internal IDeckEntry WadeDeck { get; }
+
+	private static readonly IReadOnlyList<Type> CommonCardTypes = [
+	];
+
+	private static readonly IReadOnlyList<Type> UncommonCardTypes = [
+	];
+
+	private static readonly IReadOnlyList<Type> RareCardTypes = [
+	];
+
+	private static readonly IEnumerable<Type> AllCardTypes
+		= [
+			.. CommonCardTypes,
+			.. UncommonCardTypes,
+			.. RareCardTypes,
+			// typeof(WadeExeCard),
+		];
+
+	private static readonly IReadOnlyList<Type> CommonArtifacts = [
+	];
+
+	private static readonly IReadOnlyList<Type> BossArtifacts = [
+	];
+
+	// private static readonly IReadOnlyList<Type> DuoArtifacts = [
+	// ];
+
+	private static readonly IEnumerable<Type> AllArtifactTypes
+		= [
+			.. CommonArtifacts,
+			.. BossArtifacts,
+		];
+
+	private static readonly IEnumerable<Type> RegisterableTypes
+		= [
+			.. AllCardTypes,
+			.. AllArtifactTypes,
+			typeof(ClusterMissile),
+		];
+
+	public ModEntry(IPluginPackage<IModManifest> package, IModHelper helper, ILogger logger) : base(package, helper, logger)
+	{
+		Instance = this;
+		Harmony = helper.Utilities.Harmony;
+		KokoroApi = helper.ModRegistry.GetApi<IKokoroApi>("Shockah.Kokoro")!.V2;
+
+		this.AnyLocalizations = new JsonLocalizationProvider(
+			tokenExtractor: new SimpleLocalizationTokenExtractor(),
+			localeStreamFunction: locale => package.PackageRoot.GetRelativeFile($"i18n/{locale}.json").OpenRead()
+		);
+		this.Localizations = new MissingPlaceholderLocalizationProvider<IReadOnlyList<string>>(
+			new CurrentLocaleOrEnglishLocalizationProvider<IReadOnlyList<string>>(this.AnyLocalizations)
+		);
+		
+		WadeDeck = helper.Content.Decks.RegisterDeck("Wade", new()
+		{
+			Definition = new() { color = new("56DF6A"), titleColor = Colors.white },
+			DefaultCardArt = StableSpr.cards_colorless,
+			BorderSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/CardFrame.png")).Sprite,
+			Name = this.AnyLocalizations.Bind(["character", "name"]).Localize,
+			ShineColorOverride = args => DB.decks[args.Card.GetMeta().deck].color.normalize().gain(0.5),
+		});
+
+		foreach (var type in RegisterableTypes)
+			AccessTools.DeclaredMethod(type, nameof(IRegisterable.Register))?.Invoke(null, [package, helper]);
+	}
+}
