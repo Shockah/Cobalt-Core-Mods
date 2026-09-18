16360a89a291b5297e292a1a6b1c4e25da5ad755
Michał Dolaś <me@shockah.pl>
2026-09-18 13:53:04 +0200

    NewLight: Moving Target, Pugilist, Strategist

---
+++ NewLight/Features/WeaponPerks/Strategist.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using FSPRO;
+using Nanoray.PluginManager;
+using Nickel;
+
+namespace Shockah.NewLight;
+
+internal sealed class StrategistWeaponPerk : IRegisterable
+{
+	public static ICardTraitEntry Trait { get; private set; } = null!;
+	
+	public static void Register(IPluginPackage<IModManifest> package, IModHelper helper)
+	{
+		var icon = ModEntry.Instance.Helper.Content.Sprites.RegisterSprite(ModEntry.Instance.Package.PackageRoot.GetRelativeFile("assets/CardTraits/WeaponPerks/Strategist.png"));
+		
+		Trait = ModEntry.Instance.Helper.Content.Cards.RegisterTrait("Strategist", new()
+		{
+			Icon = (_, _) => icon.Sprite,
+			Name = ModEntry.Instance.AnyLocalizations.Bind(["CardTrait", "WeaponPerk", "Strategist", "Name"]).Localize,
+			Tooltips = (_, _) =>
+			[
+				new GlossaryTooltip($"cardtrait.{ModEntry.Instance.Package.Manifest.UniqueName}::Strategist")
+				{
+					Icon = icon.Sprite,
+					TitleColor = Colors.cardtrait,
+					Title = ModEntry.Instance.Localizations.Localize(["CardTrait", "WeaponPerk", "Strategist", "Name"]),
+					Description = ModEntry.Instance.Localizations.Localize(["CardTrait", "WeaponPerk", "Strategist", "Description"]),
+				}
+			]
+		});
+
+		LegendaryWeaponCard.WeaponPerkConditions[Trait.UniqueName] = _ => true;
+		
+		helper.Events.RegisterBeforeArtifactsHook(nameof(Artifact.OnPlayerPlayCard), (Card card, State state, Combat combat) =>
+		{
+			if (!helper.Content.Cards.IsCardTraitActive(state, card, Trait))
+				return;
+			combat.Queue(new Action());
+		});
+	}
+
+	private sealed class Action : CardAction
+	{
+		public override void Begin(G g, State s, Combat c)
+		{
+			base.Begin(g, s, c);
+
+			var abilitiesToCharge = c.Abilities
+				.Where(card => Abilities.GetAbilityType(card.Key()) == Abilities.UTILITY_ABILITY)
+				.Where(card => Abilities.GetCooldown(s, c, card) > 0)
+				.ToList();
+
+			if (abilitiesToCharge.Count == 0)
+			{
+				timer = 0;
+				return;
+			}
+
+			foreach (var card in abilitiesToCharge)
+				Abilities.SetCurrentCooldown(s, c, card, Abilities.GetCurrentCooldown(s, c, card) - 1);
+			Audio.Play(Event.Status_PowerUp);
+		}
+	}
+}
